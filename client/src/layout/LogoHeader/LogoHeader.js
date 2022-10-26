bb4f1345d75ca15262f84654009a979a94817f65
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-10-26 15:20:43 +0300

    create Logo component with link (logo icon for test)

---
+++ client/src/layout/LogoHeader/LogoHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TravelExploreOutlinedIcon from '@mui/icons-material/TravelExploreOutlined';
+import { NavLink } from 'react-router-dom';
+import styles from './LogoHeader.module.scss'
+
+const LogoHeader = () =>
+(
+    <NavLink to='/' className={styles.logoLink} >
+        <TravelExploreOutlinedIcon fontSize='large' />
+    </NavLink>
+);
+
+
+export default LogoHeader;
