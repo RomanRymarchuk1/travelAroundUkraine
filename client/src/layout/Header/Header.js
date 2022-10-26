4ed249fa4ac97402f85604dcdc6c26c92defb996
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-10-26 17:57:02 +0300

    optimize Header component

--- client/src/layout/Header/Header.js
+++ client/src/layout/Header/Header.js
@@ -3,29 +3,38 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
-import TubletMenu from '../TabletMenu/TubletMenu';
+import TabletMenu from '../TabletMenu/TabletMenu';
 import LogoHeader from '../LogoHeader/LogoHeader';
 import HeaderSearchFiled from '../SearchFiled/HeaderSearchFiled';
+
+
 
 
 const Header = () => {
 
     const [scrollY, setScrollY] = useState(window.scrollY)
-
     document.addEventListener('scroll', () => setScrollY(window.scrollY))
 
+    const appBarSX = scrollY > 400 ?
+        { background: 'gray', transition: '0.3s' } :
+        { background: 'none', transition: '0.3s' };
+
+    const containerSX = {
+        px: { xs: 4, sm: 7, md: 10, lg: 20 }
+    }
+
     return (
-        <AppBar position="fixed" sx={scrollY > 400 ?
-            { background: 'gray', transition: '0.3s' } :
-            { background: 'none', transition: '0.3s' }} >
-            <Container maxWidth="xl" sx={{
-                px: { xs: 4, sm: 7, md: 10, lg: 20 }
-            }} >
+        <AppBar
+            position="fixed"
+            sx={appBarSX} >
+            <Container
+                maxWidth="xl"
+                sx={containerSX} >
                 <Toolbar disableGutters  >
                     <LogoHeader />
                     <HeaderSearchFiled />
                     <BurgerMenu />
-                    <TubletMenu />
+                    <TabletMenu />
                 </Toolbar>
             </Container>
         </AppBar >
