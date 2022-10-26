e51065719637fab9a605290d3f601078b87c85b6
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-10-26 20:15:14 +0300

    add Tour components in export

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import TourAccordion from './TourAccordion/TourAccordion';
+import TourSidebar from './TourSidebar/TourSidebar';
+import TourReasonToChoose from './TourReasonToChoose/TourReasonToChoose';
+
+export { TourAccordion, TourSidebar, TourReasonToChoose };
