1c636da7c6b9566ab73c9b3c4929575f17a5ee81
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-10-27 20:28:34 +0300

    add: useEffect hook use our prettier config

--- client/src/layout/Header/Header.js
+++ client/src/layout/Header/Header.js
@@ -1,52 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import TabletMenu from '../TabletMenu/TabletMenu';
 import LogoHeader from '../LogoHeader/LogoHeader';
-import HeaderSearchFiled from '../SearchFiled/HeaderSearchFiled';
-
-
-
+import HeaderSearchField from '../HeaderSearchField/HeaderSearchField';
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState();
 
-    const [scrollY, setScrollY] = useState(window.scrollY)
-    document.addEventListener('scroll', () => setScrollY(window.scrollY))
+  useEffect(() => document.addEventListener('scroll', () => setScrollY(window.scrollY)), []);
 
-    const appBarSX = scrollY > 400 ?
-        { background: 'gray', transition: '0.3s' } :
-        { background: 'none', transition: '0.3s' };
+  const appBarSX =
+    scrollY > 400 ? { background: 'gray', transition: '0.3s' } : { background: 'none', transition: '0.3s' };
 
-    const containerSX = {
-        px: { xs: 4, sm: 7, md: 10, lg: 20 }
-    }
+  const containerSX = {
+    px: { xs: 4, sm: 7, md: 10, lg: 20 }, //    TODO: change to Ahmed's values!
+  };
 
-    return (
-        <AppBar
-            position="fixed"
-            sx={appBarSX} >
-            <Container
-                maxWidth="xl"
-                sx={containerSX} >
-                <Toolbar disableGutters  >
-                    <LogoHeader />
-                    <HeaderSearchFiled />
-                    <BurgerMenu />
-                    <TabletMenu />
-                </Toolbar>
-            </Container>
-        </AppBar >
-    )
-
-}
-
-
-
-
-
-
-
+  return (
+    <AppBar position="fixed" sx={appBarSX}>
+      <Container maxWidth="xl" sx={containerSX}>
+        <Toolbar disableGutters>
+          <LogoHeader />
+          <HeaderSearchField />
+          <BurgerMenu />
+          <TabletMenu />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Header;
