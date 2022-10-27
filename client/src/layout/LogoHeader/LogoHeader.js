fbf4d50b43c964a2eb1bcd62d389abed08a30b16
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-10-27 20:26:39 +0300

    use our prettier config

--- client/src/layout/LogoHeader/LogoHeader.js
+++ client/src/layout/LogoHeader/LogoHeader.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import TravelExploreOutlinedIcon from '@mui/icons-material/TravelExploreOutlined';
 import { NavLink } from 'react-router-dom';
-import styles from './LogoHeader.module.scss'
+import styles from './LogoHeader.module.scss';
 
-const LogoHeader = () =>
-(
-    <NavLink
-        to='/'
-        className={styles.logoLink} >
-        <TravelExploreOutlinedIcon fontSize='large' />
-    </NavLink>
+const LogoHeader = () => (
+  <NavLink to="/" className={styles.logoLink}>
+    <TravelExploreOutlinedIcon fontSize="large" />
+  </NavLink>
 );
-
 
 export default LogoHeader;
