0317511ff39a8cd46d5c0cb8326de06ed432cc9a
Rustam Rustamov <rustam.r.r.r.r@gmail.com>
2022-10-28 00:08:39 +0300

    swiper-css-bundle-added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.module.scss';
 import AppRoutes from './AppRoutes';
+// eslint-disable-next-line import/no-unresolved
+import "swiper/css/bundle";
 
 function App() {
   return (
