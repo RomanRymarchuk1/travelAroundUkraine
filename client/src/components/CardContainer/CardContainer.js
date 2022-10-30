57c58947b18c4889d0d0e1a047614ed11d5b136a
vlad <vladislav2498@gmail.com>
2022-10-30 14:24:03 +0200

    ыцшзук 3.0

--- client/src/components/CardContainer/CardContainer.js
+++ client/src/components/CardContainer/CardContainer.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import {Swiper, SwiperSlide} from "swiper/react";
-import { Navigation} from 'swiper'
+import {Navigation} from 'swiper'
 import CardItem from '../CardItem/CardItem';
 import styles from './CardContainer.module.scss';
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-
 
 
 const cards = [
@@ -32,23 +31,31 @@ const CardContainer = () =>
             <h2 className={styles.header}>Popular tours</h2>
             <NavLink className={styles.allTours} to="/catalogue">View All</NavLink>
         </div>
-                <Swiper
-                    slidesPerView={4}
-                    spaceBetween={30}
-                    slidesPerGroup={1}
-                    loop={true}
-                    loopFillGroupWithBlank={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={true}
-                    modules={[Navigation]}
-                    className="mySwiper">
-                    {cards.map(card => (
-                        <SwiperSlide
-                        >{card}</SwiperSlide>
-                    ))}
-                </Swiper>
+        <div className={styles.swiperContainer}>
+            <div id='previous' className={styles.btnPrev}></div>
+            <div id='next' className={styles.btnNext}></div>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                slidesPerGroup={1}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={{
+                    nextEl: '#next',
+                    prevEl: '#previous',
+                }}
+                modules={[Navigation]}
+                className="mySwiper">
+                {cards.map(card => (
+                    <SwiperSlide
+                    >{card}
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     </div>
 
 export default CardContainer;
--- client/src/components/CardItem/CardItem.js
+++ client/src/components/CardItem/CardItem.js
@@ -18,3 +18,4 @@ function CardItem() {
 }
 
 export default CardItem;
+
