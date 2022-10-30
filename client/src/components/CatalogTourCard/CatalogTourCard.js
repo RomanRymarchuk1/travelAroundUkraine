99435735139697dbba1cc8cf83b97d6c2d710b5b
AlinaPalyvoda <alina.palyvoda@gmail.com>
2022-10-30 20:42:06 +0200

    feat: added CatalogTourCard

---
+++ client/src/components/CatalogTourCard/CatalogTourCard.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { Stack, CardActions, CardMedia, Button, Typography, CardContent } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import { ReactComponent as CoinsIcon } from '../../svg/CoinsIcon.svg';
+import styles from './CatalogTourCard.module.scss';
+
+const CardButton = styled(Button)(() => ({
+  height: '40px',
+  fontSize: '12px',
+  padding: '0',
+
+  '&:hover': {
+    color: 'secondary',
+  },
+}));
+
+function CatalogTourCard() {
+  return (
+    <Stack
+      direction={{ xs: 'column', tablet: 'row' }}
+      sx={{ backgroundColor: 'white', borderRadius: '10px', boxShadow: '0px 0px 40px rgba(0, 0, 0, 0.05)'}}
+      spacing={1}
+    >
+      <CardMedia
+        sx={{
+          width: {
+            xs: '100%',
+            tablet: '238px',
+          },
+          height: {
+            xs: '240px',
+            tablet: 'auto',
+          },
+          borderRadius: {
+            xs: '10px 10px 0 0',
+            tablet: '10px 0 0 10px',
+          },
+        }}
+        component="img"
+        image="https://visitukraine.today/media/tours/gallery/ALeR7GgYjAqCqfJnQX5ZYKnBcsDZ6MTJs77IIBKi.jpg"
+        alt="tour photo"
+      />
+      <CardContent sx={{ padding: '0 30px 0 36px' }}>
+        <Typography sx={{ color: 'black', marginBottom: '20px', marginTop: '20px' }} gutterBottom variant="h3" component="div">
+          Sightseeing tour of Chernivtsi
+        </Typography>
+
+        <Stack direction="row" spacing={3} alignItems="start" sx={{ marginBottom: '20px' }}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <CoinsIcon />
+            <Typography variant="h3" color="primary">
+              69 €
+            </Typography>
+          </Stack>
+
+          <Stack direction="row" spacing={1} alignItems="center">
+            <AccessTimeIcon color="primary" sx={{ height: '21px' }} />
+            <Typography variant="h3" color="primary">
+              3 hours
+            </Typography>
+          </Stack>
+        </Stack>
+
+        <Typography className={styles.tourDescription} sx={{ color: 'black' }} variant="body1" color="text.secondary">
+          Chernivtsi is often called the Ukrainian Paris, and for good reason - the pearl of Ukrainian Bukovina is in no
+          way inferior to exquisite European capitals and offers tourists entertainment for every taste. We suggest
+          starting with the classics and visiting the top must-see places that will make you fall in love with this
+          city.
+        </Typography>
+
+        <CardActions>
+          <CardButton
+            href="#"
+            sx={{
+              width: {
+                xs: '244px',
+                tablet: '160px',
+              },
+              marginTop: {
+                xs: '30px',
+                desktop: '10px',
+              },
+            }}
+          >
+            More details
+          </CardButton>
+        </CardActions>
+      </CardContent>
+    </Stack>
+  );
+}
+
+export default CatalogTourCard;
--- client/src/pages/CataloguePage/CataloguePage.js
+++ client/src/pages/CataloguePage/CataloguePage.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Stack, Container, Box, Typography } from '@mui/material';
+import CatalogTourCard from '../../components/CatalogTourCard/CatalogTourCard';
 
-const CataloguePage = () => <div>CATALOGUE</div>;
+const CataloguePage = () => (
+  <Box sx={{ backgroundColor: '#EDEDED' }}>
+    <Container>
+      <Typography sx={{ marginBottom: '40px' }} variant="h2">TOURS</Typography>
+      <Stack spacing={2}>
+        <CatalogTourCard />
+        <CatalogTourCard />
+        <CatalogTourCard />
+      </Stack>
+    </Container>
+  </Box>
+);
 
 export default CataloguePage;
