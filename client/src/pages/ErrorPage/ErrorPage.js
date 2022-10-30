b408d70a4138adbb287d54c10ec9b80b355496d4
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-10-30 09:50:10 +0200

    feat: add 404 pages and link to home

--- client/src/pages/ErrorPage/ErrorPage.js
+++ client/src/pages/ErrorPage/ErrorPage.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import styles from './ErrorPage.module.scss';
 
-const ErrorPage = () => <div>Error</div>;
+const ErrorPage = () => {
+  const { container, navigataionList } = styles;
+  return (
+    <Box className={container}>
+      <img src="./assets/images/404/heart.png" alt="uk" />
+      <Typography variant="h2">404 not found</Typography>
+      <Typography component="p">Please, navigate to main page to plan your trip</Typography>
+      <Box>
+        <Typography component="nav" className={navigataionList}>
+          <Typography component="ul">
+            <Typography component="li">
+              <Link to="/">Home</Link>
+            </Typography>
+          </Typography>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 export default ErrorPage;
