a1ad08caac6123e3d54aab182692ef1813326f9b
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-10-30 11:02:38 +0200

    refactor(Title): delete component prop & change color to match theme

--- client/src/components/TourInfoDialog/TourInfoDialog.js
+++ client/src/components/TourInfoDialog/TourInfoDialog.js
@@ -38,7 +38,7 @@ const Section = styled(MuiBox)(({ theme }) => ({
   marginBottom: 20,
 }));
 
-const Title = styled((props) => <Typography variant="h3" color="black" component="p" {...props} />)({
+const Title = styled((props) => <Typography variant="h3" color="text.primary" {...props} />)({
   textTransform: 'uppercase',
   fontWeight: 700,
   marginBottom: 20,
