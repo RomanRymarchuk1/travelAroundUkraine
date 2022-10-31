6706982ceacc68e9c454ee74e2be3142264a4831
Vladyslav <vladislav2498@gmail.com>
2022-10-31 23:07:04 +0200

    Hot Deals Section 3.0

--- client/src/components/CardContainer/CardContainer.js
+++ client/src/components/CardContainer/CardContainer.js
@@ -2,84 +2,116 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Navigation} from 'swiper'
-import CardItem from '../CardItem/CardItem';
+import CardItem from '../CardItem';
 import styles from './CardContainer.module.scss';
 
 
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+import 'swiper/swiper.min.css'
+
+
 
 
 const cards = [
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
-    <CardItem/>,
+    {
+        name: "Kyiv city walk",
+        region: "Kyiv region",
+        id: 1,
+    },
+    {
+        name: "Lviv city walk",
+        region: "Lviv region",
+        id: 2,
+    },
+    {
+        name: "Donetsk city walk",
+        region: "Donetsk region",
+        id: 3,
+    },
+    {
+        name: "Odessa city walk",
+        region: "Odessa region",
+        id: 4,
+    },
+    {
+        name: "Dnepr city walk",
+        region: "Dnepr region",
+        id: 5,
+    },
+    {
+        name: "Chernivtsi city walk",
+        region: "Chernivtsi region",
+        id: 6,
+    },
+    {
+        name: "Ternopil city walk",
+        region: "Ternopil region",
+        id: 7,
+    },
+    {
+        name: "Lutsk city walk",
+        region: "Lutsk region",
+        id: 8,
+    },
 ]
 
 
-const CardContainer = () => {
+const CardContainer = () =>
 
-    return (
-        <div className={styles.cardContainer}>
-            <div className={styles.headerContainer}>
-                <h2 className={styles.header}>Popular tours</h2>
-                <NavLink className={styles.allTours} to="/catalogue">View All</NavLink>
-            </div>
-            <div className={styles.swiperContainer}>
-                <div id='previous' className={styles.btnPrev}></div>
-                <div id='next' className={styles.btnNext}></div>
-                <Swiper
-                    breakpoints={{
-                        265: {
-                            width: 265,
-                            slidesPerView: 1,
-                        },
-                        560: {
-                            width: 560,
-                            slidesPerView: 2,
-                        },
-                        860: {
-                            width: 860,
-                            slidesPerView: 3,
-                        },
-                        1150: {
-                            width: 1150,
-                            slidesPerView: 4,
-                        },
-                    }}
-                    spaceBetween={30}
-                    slidesPerGroup={1}
-                    loop={true}
-                    loopFillGroupWithBlank={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    navigation={{
-                        nextEl: '#next',
-                        prevEl: '#previous',
-                    }}
-                    modules={[Navigation]}
-                    className="mySwiper">
-                    {cards.map(card => (
-                        <SwiperSlide
-                        >{card}
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
+    <div className={styles.cardContainer}>
+        <div className={styles.headerContainer}>
+            <h2 className={styles.header}>Popular tours</h2>
+            <NavLink className={styles.allTours} to="/catalogue">View All</NavLink>
         </div>
-    )
-}
+        <div className={styles.swiperContainer}>
+            <div id='previous' className={styles.btnPrev}> </div>
+            <div id='next' className={styles.btnNext}> </div>
+            <Swiper
+                breakpoints={{
+                    265: {
+                        width: 265,
+                        slidesPerView: 1,
+                    },
+                    560: {
+                        width: 560,
+                        slidesPerView: 2,
+                    },
+                    860: {
+                        width: 860,
+                        slidesPerView: 3,
+                    },
+                    1150: {
+                        width: 1150,
+                        slidesPerView: 4,
+                    },
+                }}
+                spaceBetween={30}
+                slidesPerGroup={1}
+                loop
+                loopFillGroupWithBlank
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={{
+                    nextEl: '#next',
+                    prevEl: '#previous',
+                }}
+                modules={[Navigation]}
+                className="mySwiper">
+                {cards.map(({name, region, id}) => (
+                    <SwiperSlide
+                        key={id}
+                    ><CardItem
+                        name={name}
+                        region={region}
+                        id={id}
+                    />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
+    </div>
+
+
 
 export default CardContainer;
 
--- client/src/components/CardItem/CardItem.js
+++ client/src/components/CardItem/CardItem.js
@@ -1,21 +1,30 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import styles from './CardItem.module.scss'
 import {ReactComponent as Arrow} from "../../svg/hotDeals/arrow.svg";
 
 
-function CardItem() {
-    return (
+
+
+const CardItem = () =>
         <div className={styles.cardItem}>
             <p className={styles.price}>$ 200.00</p>
-            <p className={styles.header}>KYIV CITY WALK</p>
+            <p className={styles.header}>Kyiv city walk</p>
             <div className={styles.location}>
                 <img src="https://visitukraine.today/assets/img/fromukraine/maps-and-flags.png" alt="location"/>
-                <p className={styles.text}>Kyiv Region</p>
+                <p className={styles.text}>Kyiv region</p>
                 <Arrow className={styles.arrow}/>
             </div>
         </div>
-    );
-}
+
+
+
+// CardItem.PropTypes = {
+//     name: PropTypes.string.isRequired,
+//     region: PropTypes.string.isRequired,
+// };
+
+
 
 export default CardItem;
 
