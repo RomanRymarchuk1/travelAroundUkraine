90d5ff06e1d3b308fcb305e99727b21065492a61
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-11-01 12:29:58 +0200

    homePage

--- client/src/layout/Footer/Footer.js
+++ client/src/layout/Footer/Footer.js
@@ -9,9 +9,8 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import { Box, Typography, Link as MUILink } from '@mui/material';
 import { Link } from 'react-router-dom';
-import classNames from 'classnames';
+import { styled } from '@mui/material/styles';
 import scrollToTop from '../../utils/scrollToTop/scrollToTop';
-import styles from './Footer.module.scss';
 
 const navigationMenu = [
   {
@@ -55,95 +54,219 @@ const footerGallery = [
   },
 ];
 
-const Footer = () => {
-  const { footer, container, image, contactsData, socialIcons, recentTrips, navigationBar, ourAwards, navigationList } =
-    styles;
-  return (
-    <Box component="footer" className={footer}>
-      <Box className={container}>
+const FooterContainer = styled('div')({
+  color: 'white',
+  backgroundColor: 'black',
+  paddingBottom: '10px',
+});
+
+const FooterWrapper = styled('div')({
+  margin: '0 auto',
+  padding: '30px',
+  maxWidth: '1200px',
+  '@media (min-width: 768px)': {
+    '& > div': {
+      display: 'flex',
+      gap: '30px',
+    },
+  },
+});
+
+const OurAwards = styled('div')({
+  '& > h3': {
+    paddingBottom: '30px',
+    color: 'white',
+  },
+  '& > p': {
+    paddingBottom: '30px',
+    color: 'white',
+  },
+});
+
+const ContactsData = styled('div')({
+  '& > h3': {
+    color: 'white',
+  },
+});
+
+const ContactsInfo = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  gap: '10px',
+  paddingTop: '30px',
+  '& > div': {
+    display: 'flex',
+    '& > a': {
+      textDecoration: 'none',
+      color: 'inherit',
+      marginLeft: '10px',
+    },
+    '& > p': {
+      marginLeft: '10px',
+      marginBottom: '20px',
+      color: 'white',
+    },
+  },
+});
+
+const SocialIcons = styled('div')({
+  display: 'flex',
+  justifyContent: 'left',
+  gap: '15px',
+  paddingTop: '20px',
+  paddingBottom: '40px',
+  '& > div': {
+    '& > svg': {
+      width: '40px',
+      height: '40px',
+      cursor: 'pointer',
+    },
+  },
+});
+
+const RecentTrips = styled('div')({
+  display: 'grid',
+  columnGap: '5px',
+  gap: '10px',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  marginBottom: '30px',
+  paddingTop: '30px',
+  '& > a >  img': {
+    width: '100%',
+    borderRadius: '5px',
+  },
+  '@media (min-width: 768px)': {
+    gridTemplateColumns: 'repeat(2, 1fr)',
+  },
+  '@media (min-width: 950px)': {
+    gridTemplateColumns: 'repeat(3, 1fr)',
+  },
+});
+
+const FooterNavigation = styled('div')({
+  borderTop: '1px solid rgba($color: #ffffff, $alpha: 0.5)',
+  margin: '0 auto',
+  padding: '30px',
+  maxWidth: '1200px',
+  '@media (min-width: 768px)': {
+    display: 'flex',
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+    '& > *': { display: 'flex', gap: '30px' },
+  },
+});
+
+const NavigationList = styled('ul')({
+  display: 'flex',
+  justifyContent: 'left',
+  alignItems: 'center',
+  gap: '20px',
+  padding: '0',
+  paddingBottom: '20px',
+  '& > li': {
+    listStyleType: 'none',
+    ' & > a': {
+      textDecoration: 'none',
+      color: 'white',
+      '&:hover': {
+        color: 'blue',
+      },
+    },
+  },
+  '@media *min-width: 768px': {
+    paddingBottom: '0',
+  },
+});
+
+const Footer = () => (
+  <FooterContainer>
+    <FooterWrapper>
+      <Box>
+        <OurAwards>
+          <Typography variant="h3">Our Awards</Typography>
+          <Typography>
+            Ukraine is a country of brave people and countless opportunities. Here you will be accepted as a relative,
+            surrounded by care as at home and get everything done as one would do it for themselves
+          </Typography>
+          <Typography
+            sx={{ width: '250px', marginTop: '30px', paddingBottom: '40px' }}
+            component="img"
+            src="./assets/images/footer/awards.png"
+            alt="awards_mage"
+          />
+        </OurAwards>
+        <ContactsData>
+          <Typography variant="h3">Contact info</Typography>
+          <ContactsInfo>
+            <Box>
+              <SmartphoneOutlinedIcon color="white" />
+              <MUILink href="tel:+380661482997">+38 (066) 148 29 97</MUILink>
+            </Box>
+            <Box>
+              <LocationOnOutlinedIcon color="white" />
+              <MUILink
+                rel="noreferrer"
+                target="_blank"
+                href="https://www.google.com.ua/maps/place/%D0%9C%D0%B0%D1%80%D0%B8%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9+%D0%B4%D0%B2%D0%BE%D1%80%D0%B5%D1%86/@50.4438071,30.544801,15.25z/data=!4m5!3m4!1s0x40d4cfad45d00d9d:0xed19f61774e5a260!8m2!3d50.4483553!4d30.5376094?hl=ru"
+              >
+                Mariinsky palace, Kyiv, Ukraine
+              </MUILink>
+            </Box>
+            <Box>
+              <TimerOutlinedIcon />
+              <Typography>Mon - Sat 8.00 - 18.00 Sunday CLOSED</Typography>
+            </Box>
+            <SocialIcons>
+              <Box>
+                <FacebookRoundedIcon />
+              </Box>
+              <Box>
+                <TwitterIcon />
+              </Box>
+              <Box>
+                <YouTubeIcon />
+              </Box>
+              <Box>
+                <PinterestIcon />
+              </Box>
+              <Box>
+                <InstagramIcon />
+              </Box>
+            </SocialIcons>
+          </ContactsInfo>
+        </ContactsData>
         <Box>
-          <Box className={ourAwards}>
-            <Typography variant="h3">Our Awards</Typography>
-            <Typography paragraph="true" variant="subtitle1">
-              Ukraine is a country of brave people and countless opportunities. Here you will be accepted as a relative,
-              surrounded by care as at home and get everything done as one would do it for themselves
-            </Typography>
-            <Typography component="img" className={image} src="./assets/images/footer/awards.png" alt="awards_mage" />
-          </Box>
-          <Box>
-            <Typography variant="h3">Contact info</Typography>
-            <Box className={contactsData}>
-              <Box>
-                <SmartphoneOutlinedIcon color="white" />
-                <MUILink href="tel:+380661482997">+38 (066) 148 29 97</MUILink>
-              </Box>
-              <Box>
-                <LocationOnOutlinedIcon color="white" />
-                <MUILink
-                  rel="noreferrer"
-                  target="_blank"
-                  href="https://www.google.com.ua/maps/place/%D0%9C%D0%B0%D1%80%D0%B8%D0%B8%D0%BD%D1%81%D0%BA%D0%B8%D0%B9+%D0%B4%D0%B2%D0%BE%D1%80%D0%B5%D1%86/@50.4438071,30.544801,15.25z/data=!4m5!3m4!1s0x40d4cfad45d00d9d:0xed19f61774e5a260!8m2!3d50.4483553!4d30.5376094?hl=ru"
-                >
-                  Mariinsky palace, Kyiv, Ukraine
-                </MUILink>
-              </Box>
-              <Box>
-                <TimerOutlinedIcon />
-                <Typography paragraph="true" variant="subtitle1">
-                  Mon - Sat 8.00 - 18.00 Sunday CLOSED
-                </Typography>
-              </Box>
-              <Box className={socialIcons}>
-                <Box>
-                  <FacebookRoundedIcon />
-                </Box>
-                <Box>
-                  <TwitterIcon />
-                </Box>
-                <Box>
-                  <YouTubeIcon />
-                </Box>
-                <Box>
-                  <PinterestIcon />
-                </Box>
-                <Box>
-                  <InstagramIcon />
-                </Box>
-              </Box>
-            </Box>
-          </Box>
-          <Box>
-            <Typography variant="h3">Recent Trips</Typography>
-            <Box className={recentTrips}>
-              {footerGallery.map((el) => (
-                <Link to="/catalogue">
-                  <Typography component="img" src={el.imagePath} alt={el.altDescription} />
-                </Link>
-              ))}
-            </Box>
-          </Box>
+          <Typography sx={{ color: 'white' }} variant="h3">
+            Recent Trips
+          </Typography>
+          <RecentTrips>
+            {footerGallery.map((el) => (
+              <Link to="/catalogue">
+                <Typography component="img" src={el.imagePath} alt={el.altDescription} />
+              </Link>
+            ))}
+          </RecentTrips>
         </Box>
       </Box>
-      <Box className={classNames(navigationBar, container)}>
-        <Box>
-          <Typography component="nav">
-            <Typography className={navigationList} component="ul">
-              {navigationMenu.map((el) => (
-                <Typography key={el.name} component="li">
-                  <Link onClick={() => scrollToTop('smooth')} to={el.path}>
-                    {el.name}
-                  </Link>
-                </Typography>
-              ))}
-            </Typography>
-          </Typography>
-        </Box>
-        <Typography paragraph="true" variant="subtitle1">
-          © Copyright travel around ukraine project team
+    </FooterWrapper>
+    <FooterNavigation>
+      <Box>
+        <Typography component="nav">
+          <NavigationList>
+            {navigationMenu.map((el) => (
+              <Typography key={el.name} component="li">
+                <Link onClick={() => scrollToTop('smooth')} to={el.path}>
+                  {el.name}
+                </Link>
+              </Typography>
+            ))}
+          </NavigationList>
         </Typography>
       </Box>
-    </Box>
-  );
-};
+      <Typography sx={{ color: 'white' }}>© Copyright travel around ukraine project team</Typography>
+    </FooterNavigation>
+  </FooterContainer>
+);
 
 export default Footer;
