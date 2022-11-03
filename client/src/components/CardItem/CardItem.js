a1702df40b6482b14f49eca0e60bf40e7d53a694
Vladyslav <vladislav2498@gmail.com>
2022-11-03 18:40:59 +0200

    Hot Deals Section 5.1 (Hot fixes) (not for pull request)

--- client/src/components/CardItem/CardItem.js
+++ client/src/components/CardItem/CardItem.js
@@ -1,20 +1,96 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import {Box, Typography} from "@mui/material";
+import {Typography, Box, styled} from "@mui/material";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import styles from './CardItem.module.scss'
+
+const CardItemWrapper = styled("div")({
+    minWidth: "265px",
+    maxWidth: "265px",
+    height: "400px",
+    background: "url('../../../public/assets/images/hotDeals/aerial-view-of-colorful-mixed-forest-shrouded-in-morning-fog-on-a-beautiful-autumn-day.jpg')",
+    backgroundSize: "cover",
+    color: "white",
+    position: "relative",
+    cursor: "pointer",
+
+    "&::after": {
+        content: "''",
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        background: "rgba(0,0,0,0.3)",
+        left: "0",
+        top: "0",
+        transition: "opacity 0.5s",
+        opacity: "1",
+    },
+
+    "&:hover::after": {
+        opacity: "0",
+        cursor: "pointer",
+    }
+})
+
+const CardItemPrice = styled('div')({
+    backgroundColor: "#007aff",
+    textAlign: "center",
+    width: "108px",
+    fontSize: "18px",
+    paddingTop: "19px",
+    paddingBottom: "19px",
+    fontWeight: "bold",
+    letterSpacing: "1px",
+    marginTop: "0",
+    position: "absolute",
+    zIndex: "1",
+})
+
+const CardItemHeader = styled("div")({
+    position: "absolute",
+    fontSize: "20px",
+    fontWeight: "bold",
+    marginTop: "265px",
+    paddingLeft: "25px",
+    zIndex: "2",
+    textTransform: "uppercase",
+})
+
+const CardItemLocation = styled('div')({
+    display: "flex",
+    paddingLeft: "25px",
+    paddingRight: "25px",
+    zIndex: "2",
+    position: "absolute",
+    width: "100%",
+    alignItems: "center",
+    marginTop: "350px",
+})
+
+const CardItemText = styled('div')(
+    {
+        fontSize: "14px",
+        margin: "0",
+    }
+)
 
 
 const CardItem = () =>
-    <Box className={styles.cardItem}>
-        <Typography color="common.white" className={styles.price}>$ 200.00</Typography>
-        <Typography color="common.white" variant="h3" className={styles.header}>Kyiv city walk</Typography>
-        <div className={styles.location}>
-            <img src="https://visitukraine.today/assets/img/fromukraine/maps-and-flags.png" alt="location"/>
-            <Typography color="common.white" className={styles.text}>Kyiv region</Typography>
-            <ArrowForwardIosIcon className={styles.arrow} fontSize="small"/>
-        </div>
-    </Box>
+    <CardItemWrapper>
+        <CardItemPrice>
+            <Typography color="common.white">$ 200.00</Typography>
+        </CardItemPrice>
+        <CardItemHeader>
+            <Typography color="common.white" variant="h3">Kyiv city walk</Typography>
+        </CardItemHeader>
+        <CardItemLocation>
+            <Box component="img" src="https://visitukraine.today/assets/img/fromukraine/maps-and-flags.png"
+                 alt="location" sx={{paddingRight: "10px"}}/>
+            <CardItemText>
+                <Typography color="common.white">Kyiv region</Typography>
+            </CardItemText>
+            <ArrowForwardIosIcon sx={{marginLeft: "80px"}} fontSize="small"/>
+        </CardItemLocation>
+    </CardItemWrapper>
 
 
 // CardItem.PropTypes = {
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,4 +4,4 @@ import TourAccordion from './TourAccordion/TourAccordion';
 import TourInfoDialog from './TourInfoDialog/TourInfoDialog';
 import TourReasonToChoose from './TourReasonToChoose/TourReasonToChoose';
 
-export { TourAccordion, TourInfoDialog, TourReasonToChoose,ImageCarousel, ErrorBoundary };
+export {TourAccordion, TourInfoDialog, TourReasonToChoose, ImageCarousel, ErrorBoundary};
