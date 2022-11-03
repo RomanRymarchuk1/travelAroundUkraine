42aa62770c7b709f8bf7748371dccf34bd9e2f4b
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-03 22:55:18 +0200

    feat(test TourPage): mock mui components

--- client/src/pages/TourPage/TourPage.test.js
+++ client/src/pages/TourPage/TourPage.test.js
@@ -3,6 +3,13 @@ import TourPage from './TourPage';
 
 import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 
+jest.mock('@mui/material/Box', () => 'div');
+jest.mock('@mui/material/Stack', () => 'div');
+jest.mock('@mui/material/Container', () => 'div');
+jest.mock('@mui/material/Typography', () => 'p');
+jest.mock('@mui/material/Button', () => 'button');
+jest.mock('../../components/TourAccordion/TourAccordion.js', () => () => <div>TourAccordion</div>);
+
 describe('Tour Page snapshot testing', () => {
   test('should Tour Page match snapshot', () => {
     const { asFragment } = render(<TourPage />);
