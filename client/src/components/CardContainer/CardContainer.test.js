789a428a35ea2567100dd5a2d7960b9ed6120303
Vladyslav <vladislav2498@gmail.com>
2022-11-08 15:12:36 +0200

    Hot Deals Section Tests

--- client/src/components/CardContainer/CardContainer.test.js
+++ client/src/components/CardContainer/CardContainer.test.js
@@ -1,9 +1,13 @@
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import CardContainer from './CardContainer'
+import {MemoryRouter, Router} from 'react-router-dom'
+import React from "react";
+
+const RouterWrapper = ({children}) => <MemoryRouter>{children}</MemoryRouter>;
 
 describe('Card Container Snapshot test', () => {
     test('should CardContainer match snapshot', () => {
-        const { asFragment } = render(<CardContainer />);
+        const {asFragment} = render(<CardContainer/>, {wrapper: RouterWrapper});
         expect(asFragment()).toMatchSnapshot();
     });
 });
