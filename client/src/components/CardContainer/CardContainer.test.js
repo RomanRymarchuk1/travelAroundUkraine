99b18cb2e9ac6bac787bbdb16f28fb3d813b20d9
vlad <vladislav2498@gmail.com>
2022-11-08 19:33:20 +0200

    hot-deals tests

--- client/src/components/CardContainer/CardContainer.test.js
+++ client/src/components/CardContainer/CardContainer.test.js
@@ -1,8 +1,12 @@
-import { render } from '@testing-library/react';
-import CardContainer from './CardContainer'
 
-describe('Card Container Snapshot test', () => {
-    test('should CardContainer match snapshot', () => {
+import { render } from '@testing-library/react';
+import CardContainer from './CardContainer';
+
+jest.mock('@mui/material/Container', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/Typography', () => ({ children }) => <h2>{children}</h2>);
+
+describe('CardContainer snapshot testing', () => {
+    test('should render CardContainer component', () => {
         const { asFragment } = render(<CardContainer />);
         expect(asFragment()).toMatchSnapshot();
     });
