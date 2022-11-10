cdda524d1ded33c6b66872a745781d274110ae43
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-10 12:50:06 +0200

    hotfix: fixed ESlint errors - added eslint rule for whole file - imported memoryRouter before CardContainer

--- client/src/components/CardContainer/CardContainer.test.js
+++ client/src/components/CardContainer/CardContainer.test.js
@@ -1,16 +1,16 @@
-import React from "react";
-import {render} from '@testing-library/react';
-import CardContainer from './CardContainer'
-import {MemoryRouter} from 'react-router-dom'
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import CardContainer from './CardContainer';
 
-const RouterWrapper = ({children}) => <MemoryRouter>{children}</MemoryRouter>;
+const RouterWrapper = ({ children }) => <MemoryRouter>{children}</MemoryRouter>;
 
 jest.mock('@mui/material/Typography', () => ({ children }) => <h2>{children}</h2>);
 
-
 describe('Card Container Snapshot test', () => {
-    test('should CardContainer match snapshot', () => {
-        const {asFragment} = render(<CardContainer/>, {wrapper: RouterWrapper});
-        expect(asFragment()).toMatchSnapshot();
-    });
+  test('should CardContainer match snapshot', () => {
+    const { asFragment } = render(<CardContainer />, { wrapper: RouterWrapper });
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
