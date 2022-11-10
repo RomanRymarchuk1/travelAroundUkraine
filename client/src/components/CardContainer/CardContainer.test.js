4de077c3e5befd57d94f0c1b341ee4a7baceb17e
Vladyslav <vladislav2498@gmail.com>
2022-11-10 16:01:53 +0200

    Hot Deals Section Tests fixed

--- client/src/components/CardContainer/CardContainer.test.js
+++ client/src/components/CardContainer/CardContainer.test.js
@@ -1,16 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { render } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import {render} from '@testing-library/react';
+import {MemoryRouter} from 'react-router-dom';
 import CardContainer from './CardContainer';
 
-const RouterWrapper = ({ children }) => <MemoryRouter>{children}</MemoryRouter>;
+const RouterWrapper = ({children}) => <MemoryRouter>{children}</MemoryRouter>;
 
-jest.mock('@mui/material/Typography', () => ({ children }) => <h2>{children}</h2>);
+jest.mock('@mui/material/Typography', () => ({children}) => <h2>{children}</h2>);
+jest.mock('@mui/material/Container', () => ({children}) => <div>{children}</div>);
 
 describe('Card Container Snapshot test', () => {
-  test('should CardContainer match snapshot', () => {
-    const { asFragment } = render(<CardContainer />, { wrapper: RouterWrapper });
-    expect(asFragment()).toMatchSnapshot();
-  });
+    test('should CardContainer match snapshot', () => {
+        const {asFragment} = render(<CardContainer/>, {wrapper: RouterWrapper});
+        expect(asFragment()).toMatchSnapshot();
+    });
 });
--- client/src/components/CardItem/CardItem.test.js
+++ client/src/components/CardItem/CardItem.test.js
@@ -1,9 +1,14 @@
-import { render } from '@testing-library/react';
+import React from "react";
+import {render} from '@testing-library/react';
 import CardItem from './CardItem'
+
+
+jest.mock('@mui/material/Typography', () => ({children}) => <h2>{children}</h2>);
+jest.mock('@mui/material/Box', () => ({children}) => <div>{children}</div>);
 
 describe('Card Item Snapshot test', () => {
     test('should CardContainer match snapshot', () => {
-        const { asFragment } = render(<CardItem />);
+        const {asFragment} = render(<CardItem/>);
         expect(asFragment()).toMatchSnapshot();
     });
 });
