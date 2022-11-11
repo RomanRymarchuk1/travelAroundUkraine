e3569008253f589ecb6f5f3954c17b3bf2d6d55c
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-11 23:45:55 +0200

    feat: add delete modal

--- client/src/features/Cart/components/DeleteItemModal/DeleteItemModal.js
+++ client/src/features/Cart/components/DeleteItemModal/DeleteItemModal.js
@@ -1,8 +1,54 @@
+import {
+  Button as MuiButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  styled,
+  Typography,
+} from '@mui/material';
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const DeleteItemModal = () => <div>Delete</div>;
+const Button = styled(MuiButton)({
+  padding: '14px 24px',
+});
 
-DeleteItemModal.propTypes = {};
+const DeleteItemModal = ({ open, onClose, onDelete, tourTitle }) => (
+  <Dialog
+    open={open}
+    onClose={onClose}
+    aria-labelledby="delete-dialog-title"
+    aria-describedby="delete-dialog-description"
+  >
+    <DialogTitle id="delete-dialog-title">Delete this tour?</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="delete-dialog-description">
+        Are you sure you want to delete tour:{' '}
+        <Typography component="span" fontStyle="italic">
+          {tourTitle}
+        </Typography>
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button onClick={onClose}>Cancel</Button>
+      <Button onClick={onDelete} autoFocus>
+        Delete
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+DeleteItemModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  tourTitle: PropTypes.string,
+  onClose: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+DeleteItemModal.defaultProps = {
+  tourTitle: '',
+};
 
 export default DeleteItemModal;
