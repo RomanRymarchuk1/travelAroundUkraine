7df70748bdf6cbc23e5ae8fd6d896ce6a3f0b942
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-11 17:54:41 +0200

    feat: create CartPage component

--- client/src/pages/CartPage/CartPage.js
+++ client/src/pages/CartPage/CartPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { Box, Container, Typography } from '@mui/material';
+
 function CartPage() {
-  return <div>CART</div>;
+  return (
+    <Box mt="80px">
+      <Container>
+        <Typography variant="h1">Cart</Typography>
+        <Box>Box</Box>
+      </Container>
+    </Box>
+  );
 }
 
 export default CartPage;
