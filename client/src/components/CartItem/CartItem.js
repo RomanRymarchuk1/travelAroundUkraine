8988c84dd34349e328e90b30be1efbb565a06dc5
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-11 17:53:10 +0200

    feat: create CartItem component

---
+++ client/src/components/CartItem/CartItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+
+const CartItem = () => <Box>CartItem</Box>;
+
+CartItem.propTypes = {};
+
+export default CartItem;
