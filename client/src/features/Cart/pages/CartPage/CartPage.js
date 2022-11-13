dcd1f70a49edbc090d90572151eee840fde13b3b
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-13 16:26:37 +0200

    feat: create cart page layout

--- client/src/features/Cart/pages/CartPage/CartPage.js
+++ client/src/features/Cart/pages/CartPage/CartPage.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Stack, styled, Typography } from '@mui/material';
 import { CartItem } from '../../components';
+import TotalInfoDialog from '../../components/TotalInfoDialog/TotalInfoDialog';
+
+const ContentWrapper = styled(Stack)(({ theme }) => ({
+  gap: theme.spacing(2),
+
+  [theme.breakpoints.up('laptop')]: {
+    flexDirection: 'row',
+  },
+}));
 
 function CartPage() {
   return (
-    <Box marginY="80px">
+    <Box marginTop="80px" marginBottom="40px">
       <Container>
         <Typography variant="h1">Cart</Typography>
-        <Box>
-          <CartItem />
-        </Box>
+        <ContentWrapper>
+          <Stack gap={2} flexGrow={1}>
+            {/* For demonstration purpose only */}
+            <CartItem />
+            <CartItem />
+            <CartItem />
+          </Stack>
+          <Box flexGrow={1}>
+            <TotalInfoDialog />
+          </Box>
+        </ContentWrapper>
       </Container>
     </Box>
   );
