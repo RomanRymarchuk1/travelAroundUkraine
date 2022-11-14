294fbebdf04e956c774c5e8a592e89ef2067a545
AlinaPalyvoda <alina.palyvoda@gmail.com>
2022-11-14 04:39:40 +0200

    - update CatalogMainFilter snapshot test, - added OutlinedToggleButton snapshot test

--- client/src/components/CatalogMainFilter/CatalogMainFilter.test.js
+++ client/src/components/CatalogMainFilter/CatalogMainFilter.test.js
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 import CatalogMainFilter from './CatalogMainFilter';
 
 jest.mock('@mui/material/Button', () => ({ children }) => <button>{children}</button>);
-jest.mock('@mui/material/Box', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/Stack', () => ({ children }) => <div>{children}</div>);
 jest.mock('@mui/material/Typography', () => ({ children }) => <h3>{children}</h3>);
 jest.mock('@mui/material/Unstable_Grid2', () => ({ children }) => <div>{children}</div>);
 
