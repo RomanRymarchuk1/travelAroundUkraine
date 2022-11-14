b1da363242042e03070037aa1de234dd26f6cea8
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-14 15:29:50 +0200

    hotfix: snapshot test fix - mocked components to achieve clean unit test

--- client/src/components/CatalogFilterRegion/CatalogFilterRegion.test.js
+++ client/src/components/CatalogFilterRegion/CatalogFilterRegion.test.js
@@ -5,6 +5,8 @@ jest.mock('@mui/material/Box', () => ({ children }) => <div>{children}</div>);
 jest.mock('@mui/material/Checkbox', () => ({ children }) => <span>{children}</span>);
 jest.mock('@mui/material/FormControlLabel', () => ({ children }) => <label>{children}</label>);
 
+jest.mock('../FilterAccordion/FilterAccordion', () => ({ children }) => <div>{children}</div>);
+
 describe('CatalogFilterRegion snapshot test', () => {
   test('should CatalogFilterRegion match snapshot', () => {
     const { asFragment } = render(<CatalogFilterRegion />);
