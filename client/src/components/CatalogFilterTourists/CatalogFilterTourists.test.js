a26cefe0b50a4280774003bf2d096b17d7e16cb0
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-14 15:26:28 +0200

    hotfix: snapshot test fix - mocked components to achieve clean unit test

--- client/src/components/CatalogFilterTourists/CatalogFilterTourists.test.js
+++ client/src/components/CatalogFilterTourists/CatalogFilterTourists.test.js
@@ -4,6 +4,9 @@ import CatalogFilterTourists from './CatalogFilterTourists';
 jest.mock('@mui/material/ToggleButton', () => ({ children }) => <button>{children}</button>);
 jest.mock('@mui/material/Unstable_Grid2', () => ({ children }) => <div>{children}</div>);
 
+jest.mock('../FilterAccordion/FilterAccordion', () => ({ children }) => <div>{children}</div>);
+jest.mock('../OutlinedToggleButton/OutlinedToggleButton', () => () => <button>test button</button>);
+
 describe('CatalogFilterTourists snapshot test', () => {
   test('should CatalogFilterTourists match snapshot', () => {
     const { asFragment } = render(<CatalogFilterTourists />);
