13f7e4a6f1cb8337644f1b300f1cd1a2bb83fd56
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-14 16:13:48 +0200

    test: add snapshot test for CartItem

---
+++ client/src/features/Cart/components/CartItem/CartItem.test.js
@@ -0,0 +1,18 @@
+import { render } from '@testing-library/react';
+import CartItem from './CartItem';
+
+jest.mock('@mui/material/Stack', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/CardActions', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/CardMedia', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/Button', () => ({ children }) => <button>{children}</button>);
+jest.mock('@mui/material/Typography', () => ({ children }) => <p>{children}</p>);
+jest.mock('@mui/material/CardContent', () => ({ children }) => <div>{children}</div>);
+jest.mock('@mui/material/IconButton', () => ({ children }) => <span>{children}</span>);
+jest.mock('@mui/material/TextField', () => ({ children }) => <div>{children}</div>);
+
+describe('CartItem snapshot test', () => {
+  test('should CartItem match snapshot', () => {
+    const { asFragment } = render(<CartItem />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
