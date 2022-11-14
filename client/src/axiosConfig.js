8203909112e8aae4db2519712b825b0b5bf17c4b
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-11-14 13:54:38 +0200

    fix: rename axiosConfig

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const instance = axios.create({
+const axiosConfig = axios.create({
   baseURL: 'http://localhost:5000',
 });
 
-export default instance;
+export default axiosConfig;
