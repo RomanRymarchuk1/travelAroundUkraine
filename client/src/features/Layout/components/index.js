1b3fadb26c698f3492ef45740153bde47c09c32a
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-17 01:40:50 +0200

    feat: change file structure - delete files from old locations

--- client/src/features/Layout/components/index.js
+++
@@ -1,9 +0,0 @@
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import BurgerMenu from './BurgerMenu/BurgerMenu';
-import HeaderSearchField from './HeaderSearchField/HeaderSearchField';
-import LogoHeader from './LogoHeader/LogoHeader';
-import TabletMenu from './TabletMenu/TabletMenu';
-import HeaderFilterItems from './HeaderFilterItems/HeaderFilterItems';
-
-export { Header, Footer, BurgerMenu, HeaderSearchField, LogoHeader, TabletMenu, HeaderFilterItems };
--- client/src/features/index.js
+++
@@ -1,8 +0,0 @@
-import CartPage from './Cart/pages/CartPage/CartPage';
-import HomePage from './Home/pages/HomePage/HomePage';
-import ErrorPage from './Error/pages/ErrorPage/ErrorPage';
-import CataloguePage from './Catalogue/pages/CataloguePage/CataloguePage';
-import AppLayout from './Layout/pages/AppLayout/AppLayout';
-import TourPage from './Tour/pages/TourPage/TourPage';
-
-export { CartPage, HomePage, ErrorPage, CataloguePage, AppLayout, TourPage };
