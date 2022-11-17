440e40e60c711a7d95ae0869e75821c18bc754b7
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-17 15:21:07 +0200

    feat: signup page created - SignupPage component created - exported from pages/index.js - page added to AppRoutes

--- client/src/AppRoutes.js
+++ client/src/AppRoutes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { TourPage, HomePage, CataloguePage, ErrorPage, CheckoutPage } from './pages';
+import { TourPage, HomePage, CataloguePage, ErrorPage, CheckoutPage, SignupPage } from './pages';
 import CartPage from './features';
 import Layout from './layout/Layout/Layout';
 
 // TODO: add navigation from tour page to catalogue page
+// TODO: add protected routes after incorporating sign in feature
 
 const AppRoutes = () => (
   <Routes>
@@ -14,6 +15,7 @@ const AppRoutes = () => (
       <Route path="/tour/:tourId" element={<TourPage />} />
       <Route path="/cart" element={<CartPage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/signup" element={<SignupPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   </Routes>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,5 +3,6 @@ import HomePage from './HomePage/HomePage';
 import CataloguePage from './CataloguePage/CataloguePage';
 import ErrorPage from './ErrorPage/ErrorPage';
 import CheckoutPage from './CheckoutPage/CheckoutPage';
+import SignupPage from './SignupPage/SignupPage';
 
-export { TourPage, HomePage, CataloguePage, ErrorPage, CheckoutPage };
+export { TourPage, HomePage, CataloguePage, ErrorPage, CheckoutPage, SignupPage };
