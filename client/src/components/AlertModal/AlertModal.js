0b156fcab7541c97bd55f7ae84d40bb1c71c7096
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-11-18 23:06:55 +0200

    feat: create alert modal

---
+++ client/src/components/AlertModal/AlertModal.js
@@ -0,0 +1,51 @@
+import {
+  Button as MuiButton,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  styled,
+} from '@mui/material';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = styled(MuiButton)({
+  padding: '12px 24px',
+});
+
+const AlertModal = ({ open, onClose, onSubmit, title, children }) => (
+  <Dialog
+    open={open}
+    onClose={onClose}
+    aria-labelledby="alert-dialog-title"
+    aria-describedby="alert-dialog-description"
+  >
+    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+    <DialogContent>
+      <DialogContentText id="alert-dialog-description">{children}</DialogContentText>
+    </DialogContent>
+    <DialogActions sx={{ paddingBottom: 2 }}>
+      <Button onClick={onClose} disableElevation>
+        Cancel
+      </Button>
+      <Button onClick={onSubmit} disableElevation autoFocus>
+        Submit
+      </Button>
+    </DialogActions>
+  </Dialog>
+);
+
+AlertModal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  title: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+};
+
+AlertModal.defaultProps = {
+  title: '',
+};
+
+export default AlertModal;
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import AlertModal from './AlertModal/AlertModal';
+
+// ! delete rule when there would be more than one export
+// eslint-disable-next-line import/prefer-default-export
+export { AlertModal };
