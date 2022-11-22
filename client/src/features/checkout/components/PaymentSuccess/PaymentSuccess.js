421fd25a8daca9a8d60388dd716313af1ea86a7b
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-23 00:33:41 +0200

    update: PaymentSuccess in checkout form - utilized the AlertModal with all needed props

--- client/src/features/checkout/components/PaymentSuccess/PaymentSuccess.js
+++ client/src/features/checkout/components/PaymentSuccess/PaymentSuccess.js
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import { Button, Dialog, DialogContent, Typography, Box, DialogActions, DialogTitle } from '@mui/material';
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import { AlertModal } from '../../../../components';
 
 const PaymentSuccess = ({ activeStep, steps }) => {
   const [isOpen, setIsOpen] = useState(true);
@@ -13,27 +14,27 @@ const PaymentSuccess = ({ activeStep, steps }) => {
 
   if (activeStep !== steps.length) return null;
 
+  const titleJsx = (
+    <Box display="flex" justifyContent="center" alignItems="center" columnGap="5px">
+      <Typography variant="h2" sx={{ color: 'success.main', mb: 0 }}>
+        Payment Successfull
+      </Typography>
+      <CheckCircleOutlineRoundedIcon fontSize="large" color="success" />
+    </Box>
+  );
+
   return (
-    <Dialog open={isOpen} onClose={handleCloseBttn}>
-      <DialogTitle>
-        <Box display="flex" justifyContent="center" alignItems="center" columnGap="5px">
-          <Typography variant="h2" sx={{ color: 'success.main', mb: 0 }}>
-            Payment Successfull
-          </Typography>
-          <CheckCircleOutlineRoundedIcon fontSize="large" color="success" />
-        </Box>
-      </DialogTitle>
-
-      <DialogContent>
-        <Typography>Your order number #425435 will be shipped shortly.</Typography>
-      </DialogContent>
-
-      <DialogActions>
-        <Button onClick={handleCloseBttn} autoFocus>
-          Great !
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <AlertModal
+      open={isOpen}
+      onClose={handleCloseBttn}
+      onSubmit={handleCloseBttn}
+      title={titleJsx}
+      disableCancelButton
+      submitButtonText="Great !"
+      success
+    >
+      <Typography>Your order number #425435 will be shipped shortly.</Typography>
+    </AlertModal>
   );
 };
 
