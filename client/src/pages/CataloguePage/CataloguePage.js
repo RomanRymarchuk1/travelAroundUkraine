e6d7aec1f238e7c188d7a7d3ee98849409f3867f
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-11-23 12:00:18 +0200

    feat: 1 - add useEffect, server request and dispatch data to GS. 2 - add Loading rule, 3 - add conditional rendering

--- client/src/pages/CataloguePage/CataloguePage.js
+++ client/src/pages/CataloguePage/CataloguePage.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { styled, Stack, Box, Container, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { CatalogTourCard, CatalogMainSection, CatalogMainFilter } from '../../features/Catalogue/components';
+import getProducts from '../../api/getProducts';
+import { setProducts } from '../../store/slices/catalogueSlice';
 
 const FilterContainer = styled((props) => <Grid item xs={12} {...props} />)(({ theme }) => ({
   backgroundColor: theme.palette.primary.contrastText,
@@ -16,29 +19,51 @@ const FilterContainer = styled((props) => <Grid item xs={12} {...props} />)(({ t
   },
 }));
 
-const CataloguePage = () => (
-  <Box sx={{ backgroundColor: '#EDEDED', paddingBottom: '150px' }}>
-    <CatalogMainSection />
-    <Container>
-      <Grid container sx={{ mt: '60px', gap: '40px' }}>
-        <Grid item xs={12} laptop sx={{ p: 0 }}>
-          <Typography variant="h2" sx={{ textTransform: 'uppercase', mb: '25px' }}>
-            Tours
-          </Typography>
-          <Stack spacing={2}>
-            <CatalogTourCard />
-            <CatalogTourCard />
-            <CatalogTourCard />
-            <CatalogTourCard />
-          </Stack>
-        </Grid>
+const CataloguePage = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    const getData = async () => {
+      const data = await getProducts();
+      dispatch(setProducts(data));
+      setLoading(false);
+    };
+    getData();
+  }, []);
 
-        <FilterContainer>
-          <CatalogMainFilter />
-        </FilterContainer>
-      </Grid>
-    </Container>
-  </Box>
-);
+  return (
+    <>
+      {loading === false ? (
+        <Box sx={{ backgroundColor: '#EDEDED', paddingBottom: '150px' }}>
+          <CatalogMainSection />
+          <Container>
+            <Grid container sx={{ mt: '60px', gap: '40px' }}>
+              <Grid item xs={12} laptop sx={{ p: 0 }}>
+                <Typography variant="h2" sx={{ textTransform: 'uppercase', mb: '25px' }}>
+                  Tours
+                </Typography>
+                <Stack spacing={2}>
+                  <CatalogTourCard />
+                  <CatalogTourCard />
+                  <CatalogTourCard />
+                  <CatalogTourCard />
+                </Stack>
+              </Grid>
+              <FilterContainer>
+                <CatalogMainFilter />
+              </FilterContainer>
+            </Grid>
+          </Container>
+        </Box>
+      ) : (
+        <Typography variant="h2" sx={{ paddingTop: '400px', paddingBottom: '400px', textAlign: 'center' }}>
+          Loading...
+        </Typography>
+      )}
+      )
+    </>
+  );
+};
 
 export default CataloguePage;
