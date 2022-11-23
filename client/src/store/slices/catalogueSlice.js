768e23b29913dc0933946ca40e23d7492780217e
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-11-23 11:28:15 +0200

    fix: add exlint ignore to reducer

---
+++ client/src/store/slices/catalogueSlice.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
+import { createSlice } from '@reduxjs/toolkit';
+
+const productsSlice = createSlice({
+  name: 'products',
+  initialState: {
+    products: [],
+  },
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+  },
+});
+
+export default productsSlice.reducer;
+
+export const { setProducts } = productsSlice.actions;
