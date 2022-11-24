5769da3a57d0f253d6634986292aaae8945a34ea
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-24 20:01:21 +0200

    create redux toolkit files: - tourSlice created with async function by THUNK and reducer - reducer exported and added to reducer object in store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import tourReducer from './slices/tourSlice/tourSlice';
 
 const store = configureStore({
-  reducer: { userReducer },
+  reducer: { userReducer, tour: tourReducer },
 });
 
 export default store;
