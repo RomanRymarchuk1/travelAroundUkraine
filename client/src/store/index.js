484d10f9680d0b4a7112a94ca0944480c6dd7189
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-11-29 13:43:44 +0200

    reducer name fixed

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import tourReducer from './slices/tourSlice/tourSlice';
-import catalogueSlice from './slices/catalogueSlice/catalogueSlice';
+import catalogueReducer from './slices/catalogueSlice/catalogueSlice';
 
 const store = configureStore({
-  reducer: { userReducer, tour: tourReducer, catalogue: catalogueSlice },
+  reducer: { userReducer, tour: tourReducer, catalogue: catalogueReducer },
 });
 
 export default store;
