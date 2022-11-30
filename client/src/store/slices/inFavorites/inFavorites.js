10f84be1cc78b4d75d67ceeb3f3328e0fb674051
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-11-30 12:38:21 +0200

    fix: devide redicers to local and server side. add fetching wishlist from server

--- client/src/store/slices/inFavorites/inFavorites.js
+++ client/src/store/slices/inFavorites/inFavorites.js
@@ -1,48 +1,53 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const gettWishList = async () => {
-  try {
-    const { data } = await axios.get('/wishlist');
-    return data;
-  } catch (err) {
-    return console.warn(err);
-  }
-};
-
 const inFavoritesSlice = createSlice({
   name: 'inFavorites',
   initialState: {
     inFavorites: [],
   },
   reducers: {
-    setItemInFavorites: (state, action) => {
-      if (action.payload.isLogin) {
-        console.log('server request here');
-      } else {
-        state.inFavorites = [...state.inFavorites, action.payload.id];
-        localStorage.setItem('inFavorites', state.inFavorites);
-      }
+    setLocallyItemInFavorites: (state, action) => {
+      state.inFavorites = [...state.inFavorites, action.payload];
+      localStorage.setItem('inFavorites', state.inFavorites);
     },
-    setInitialItemsInFavorites: (state, action) => {
-      if (action.payload === false && localStorage.getItem('inFavorites') !== null) {
+    setLocallyInitialItemsInFavorites: (state) => {
+      if (localStorage.getItem('inFavorites') !== null) {
         state.inFavorites = localStorage.getItem('inFavorites').split(',');
-      } else if (action.payload === true) {
-        state.inFavorites = gettWishList();
-        localStorage.setItem('inFavorites', state.inFavorites);
       }
     },
-    deleteItemFromFavorites: (state, action) => {
-      if (action.payload.isLogin) {
-        console.log('server request here');
-      } else {
-        state.inFavorites = state.inFavorites.filter((elId) => elId !== action.payload.id);
-        localStorage.setItem('inFavorites', state.inFavorites);
-      }
+    deleteLocallyItemFromFavorites: (state, action) => {
+      state.inFavorites = state.inFavorites.filter((elId) => elId !== action.payload);
+      localStorage.setItem('inFavorites', state.inFavorites);
+    },
+    setServerItemsInFavorites: (state, action) => {
+      state.inFavorites = action.payload;
     },
   },
 });
 
-export const { setItemInFavorites, setInitialItemsInFavorites, deleteItemFromFavorites } = inFavoritesSlice.actions;
+export const {
+  setLocallyItemInFavorites,
+  setLocallyInitialItemsInFavorites,
+  deleteLocallyItemFromFavorites,
+  setServerItemsInFavorites,
+} = inFavoritesSlice.actions;
 
 export default inFavoritesSlice.reducer;
+
+export const gettWishList = async (dispatch) => {
+  try {
+    const { data, status } = await axios('/wishlist', {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    });
+    if (!status && data === null) {
+      dispatch(setServerItemsInFavorites(data));
+    } else {
+      dispatch(setServerItemsInFavorites([]));
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
