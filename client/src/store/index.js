490aadc58be9f38304af2f64258a6cd78ea55610
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-11-30 10:19:51 +0200

    feat: add favorites reducer to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import tourReducer from './slices/tourSlice/tourSlice';
 import catalogueReducer from './slices/catalogueSlice/catalogueSlice';
+import inFavoritesSlice from './slices/inFavorites/inFavorites';
 
 const store = configureStore({
-  reducer: { userReducer, tour: tourReducer, catalogue: catalogueReducer },
+  reducer: { userReducer, tour: tourReducer, catalogue: catalogueReducer, favorites: inFavoritesSlice },
 });
 
 export default store;
