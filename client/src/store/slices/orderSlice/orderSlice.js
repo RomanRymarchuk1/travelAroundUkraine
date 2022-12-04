e10ca9153226b352d0cca508594ad016ac431521
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-12-04 19:12:28 +0200

    feat: create order slice

--- client/src/store/slices/orderSlice/orderSlice.js
+++ client/src/store/slices/orderSlice/orderSlice.js
@@ -1,17 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import postNewOrder from '../../../api/postNewOrder';
 // import axios from 'axios';
 
 const initialState = {
-  data: {},
+  currentStep: 0,
+  isModalOpen: false,
+  orderInfo: {},
 };
+
+export const createNewOrder = createAsyncThunk('order/createNewOrder', async (orderData, { rejectWithValue }) => {
+  try {
+    const orderInfo = await postNewOrder(orderData);
+    return orderInfo;
+  } catch (e) {
+    return rejectWithValue(e);
+  }
+});
 
 const orderSlice = createSlice({
   name: 'order',
   initialState,
 
-  reducers: {},
+  reducers: {
+    increaseStep: (state) => {
+      state.currentStep += 1;
+    },
+    decreaseStep: (state) => {
+      if (state.currentStep === 0) {
+        state.currentStep = 0;
+      } else {
+        state.currentStep -= 1;
+      }
+    },
+    setIsModalOpen: (state, action) => {
+      state.isModalOpen = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(createNewOrder.fulfilled, (state, action) => {
+      state.orderInfo = action.payload;
+    });
+  },
 });
 
-// export const {  } = orderSlice.actions;
+export const { increaseStep, decreaseStep, setIsModalOpen } = orderSlice.actions;
 
 export default orderSlice.reducer;
