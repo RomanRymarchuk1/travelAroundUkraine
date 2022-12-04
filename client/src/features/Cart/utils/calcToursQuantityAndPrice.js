891cf4b1e72855969bda4713358b6b9c93db59f9
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-12-04 16:07:03 +0200

    feature: utility function created - function to calculate total price and quantity of all tours in cart

---
+++ client/src/features/Cart/utils/calcToursQuantityAndPrice.js
@@ -0,0 +1,14 @@
+/* eslint-disable consistent-return */
+const calcToursQuantityAndPrice = (cart) => {
+  if (cart.length) {
+    const toursQuantity = cart.map(({ cartQuantity }) => cartQuantity).reduce((total, num) => total + num);
+
+    const toursPrice = cart
+      .map(({ product: { currentPrice }, cartQuantity }) => currentPrice * cartQuantity)
+      .reduce((total, num) => total + num);
+
+    return { toursQuantity, toursPrice };
+  }
+};
+
+export default calcToursQuantityAndPrice;
