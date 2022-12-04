6dcdcf2ed7e165fc0b06a20e5b758b642322a6ba
Daniel Bliznichenko <danilbliz0201@gmail.com>
2022-12-04 19:12:14 +0200

    feat: add orderSlice to store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,9 +2,10 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
 import tourReducer from './slices/tourSlice/tourSlice';
 import catalogueReducer from './slices/catalogueSlice/catalogueSlice';
+import orderSlice from './slices/orderSlice/orderSlice';
 
 const store = configureStore({
-  reducer: { userReducer, tour: tourReducer, catalogue: catalogueReducer },
+  reducer: { userReducer, tour: tourReducer, catalogue: catalogueReducer, order: orderSlice },
 });
 
 export default store;
