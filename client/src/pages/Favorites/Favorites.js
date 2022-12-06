c40cdc34b24dc00522e18e0e19cff3f7b0658585
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-12-06 11:47:43 +0200

    feat: add checing for items which are currently in favorites. In case the cart is empty showing no items found

--- client/src/pages/Favorites/Favorites.js
+++ client/src/pages/Favorites/Favorites.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { CatalogTourCard } from '../../features/Catalogue/components';
 import { gettWishList } from '../../store/slices/inFavorites/inFavorites';
 import { setIsLoading, getProducts } from '../../store/slices/catalogueSlice/catalogueSlice';
@@ -21,27 +21,33 @@ const FavoritesPage = () => {
     dispatch(gettWishList(isLogin));
   }, [isLogin]);
   return (
-    <Grid container sx={{ mt: '60px', gap: '40px', p: 5, justifyContent: 'center' }}>
-      {products.map(({ name, currentPrice, duration, description, imageUrls, _id }) => {
-        const checkForFavorites = inFavorites.find((itemId) => _id === itemId);
-        if (checkForFavorites) {
-          return (
-            <CatalogTourCard
-              key={_id}
-              name={name}
-              description={description}
-              currentPrice={currentPrice}
-              duration={duration}
-              imageUrls={imageUrls}
-              id={_id}
-              inFavorites={checkForFavorites ? !!checkForFavorites : false}
-            />
-          );
-        }
-        return null;
-      })}
-      {/* {inFavorites.length === 1 && <h2 style={{ padding: '100px' }}>No items found</h2>} */}
-    </Grid>
+    <>
+      <Grid container sx={{ mt: '60px', gap: '40px', p: 5, justifyContent: 'center' }}>
+        {products.map(({ name, currentPrice, duration, description, imageUrls, _id }) => {
+          const checkForFavorites = inFavorites.find((itemId) => _id === itemId);
+          if (checkForFavorites) {
+            return (
+              <CatalogTourCard
+                key={_id}
+                name={name}
+                description={description}
+                currentPrice={currentPrice}
+                duration={duration}
+                imageUrls={imageUrls}
+                id={_id}
+                inFavorites={checkForFavorites ? !!checkForFavorites : false}
+              />
+            );
+          }
+          return null;
+        })}
+      </Grid>
+      {inFavorites.length === 0 && (
+        <Typography variant="h2" sx={{ padding: '100px', textAlign: 'center' }}>
+          No items found
+        </Typography>
+      )}
+    </>
   );
 };
 
