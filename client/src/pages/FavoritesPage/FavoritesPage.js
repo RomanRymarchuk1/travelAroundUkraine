075cbfc21a2ec066b3333007d5229973abab1753
Oleksandr Altynnikov <siolent.ou@gmail.com>
2022-12-08 12:21:14 +0200

    fix: rename favorites page

--- client/src/pages/FavoritesPage/FavoritesPage.js
+++ client/src/pages/FavoritesPage/FavoritesPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 import { Grid, Typography } from '@mui/material';
 import { CatalogTourCard } from '../../features/Catalogue/components';
-import { gettWishList } from '../../store/slices/inFavorites/inFavorites';
+import { gettWishList } from '../../store/slices/inFavoritesSlice/inFavorites';
 import { setIsLoading, getProducts } from '../../store/slices/catalogueSlice/catalogueSlice';
 
 const FavoritesPage = () => {
@@ -22,32 +22,35 @@ const FavoritesPage = () => {
   }, [isLogin]);
   return (
     <>
-      <Grid container sx={{ mt: '60px', gap: '40px', p: 5, justifyContent: 'center' }}>
-        {products.map(({ name, currentPrice, duration, description, imageUrls, _id }) => {
-          const checkForFavorites = inFavorites.find((itemId) => _id === itemId);
-          if (checkForFavorites) {
-            return (
-              <CatalogTourCard
-                key={_id}
-                name={name}
-                description={description}
-                currentPrice={currentPrice}
-                duration={duration}
-                imageUrls={imageUrls}
-                id={_id}
-                inFavorites={checkForFavorites ? !!checkForFavorites : false}
-                inFavoritesCounter={inFavorites.length - 1}
-              />
-            );
-          }
-          return null;
-        })}
-      </Grid>
-      {inFavorites.length === 0 && (
+      {inFavorites.length === 0 ? (
         <Typography variant="h2" sx={{ padding: '100px', textAlign: 'center' }}>
           No items found
         </Typography>
+      ) : (
+        <Grid container sx={{ mt: '60px', gap: '40px', p: 5, justifyContent: 'center' }}>
+          {products.map(({ name, currentPrice, duration, description, imageUrls, _id, itemNo }) => {
+            const checkForFavorites = inFavorites.find((itemId) => _id === itemId);
+            if (checkForFavorites) {
+              return (
+                <CatalogTourCard
+                  key={itemNo}
+                  name={name}
+                  description={description}
+                  currentPrice={currentPrice}
+                  duration={duration}
+                  imageUrls={imageUrls}
+                  itemNo={itemNo}
+                  id={_id}
+                  inFavorites={checkForFavorites ? !!checkForFavorites : false}
+                  inFavoritesCounter={inFavorites.length - 1}
+                />
+              );
+            }
+            return null;
+          })}
+        </Grid>
       )}
+      {}
     </>
   );
 };
