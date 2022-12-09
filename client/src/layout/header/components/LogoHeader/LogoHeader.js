5cc45804bf47c349db2df2a8c3f45e5a46759043
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-12-09 15:25:19 +0200

    changing the logo of our project

--- client/src/layout/header/components/LogoHeader/LogoHeader.js
+++ client/src/layout/header/components/LogoHeader/LogoHeader.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import TravelExploreOutlinedIcon from '@mui/icons-material/TravelExploreOutlined';
 import { NavLink } from 'react-router-dom';
 
 const LogoHeader = () => (
   <NavLink to="/" style={{ textDecoration: 'none' }}>
-    <TravelExploreOutlinedIcon fontSize="large" sx={{ color: 'text.primary' }} />
+    <img src="/assets/images/logo/logo.png" alt="logo" width="75" height="60" />
   </NavLink>
 );
 
