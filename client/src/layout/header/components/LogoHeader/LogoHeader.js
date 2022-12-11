c69245defc36b6ee09e1ffa27893f170f48c4439
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-12-11 14:23:54 +0200

    change logo colors

--- client/src/layout/header/components/LogoHeader/LogoHeader.js
+++ client/src/layout/header/components/LogoHeader/LogoHeader.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const LogoHeader = () => (
   <NavLink to="/" style={{ textDecoration: 'none' }}>
-    <img src="/assets/images/logo/logo.png" alt="logo" width="75" height="60" />
+    <img src="/assets/images/logo/logo.png" alt="logo" width="65" height="50" />
   </NavLink>
 );
 
