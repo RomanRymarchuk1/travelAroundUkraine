dacc1978c3b7f5963fb036cc72e2c3b1622410fb
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-12-11 16:20:44 +0200

    add reexport

---
+++ client/src/features/User/components/index.js
@@ -0,0 +1,5 @@
+import ButtonContainer from './ButtonContainer/ButtonContainer';
+import UserHeader from './UserHeader/UserHeader';
+import UserList from './UserList/UserList';
+
+export { ButtonContainer, UserHeader, UserList };
