2ab85f3f5aac6716e0625b27a8dd7c1b94c11ef0
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-12-12 14:49:26 +0200

    feature: cartSlice updated - extra reducers created - thunk functions created for all offline cart scenarios

--- client/src/store/slices/cartSlice/cartSlice.js
+++ client/src/store/slices/cartSlice/cartSlice.js
@@ -1,7 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 import axiosConfig from '../../../axiosConfig';
+import getProduct from '../../../api/getProduct';
+import updateCart from '../../../api/updateCart';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -10,8 +13,39 @@ const cartSlice = createSlice({
     isLoading: false,
   },
   reducers: {
+    // the next reducers deals with 2 types of payload type, where array or object, maybe refactoring is needed by splitting reducers and creating a second reducer to deal with local cart data
+
+    // TODO need to check naming schema of all reducer and thunk api functions
+
     setCart: (state, action) => {
-      state.data = action.payload;
+      if (Array.isArray(action.payload)) {
+        state.data = action.payload;
+      } else {
+        const index = state.data.findIndex((item) => item.product._id === action.payload._id);
+
+        if (index === -1) {
+          state.data.push({ product: { ...action.payload }, cartQuantity: 1 });
+          localStorage.setItem('cart', JSON.stringify(state.data));
+        } else {
+          state.data[index].cartQuantity += 1;
+          // refactor this function, maybe write a function in API folder that writes cart to local storage
+          localStorage.setItem('cart', JSON.stringify(state.data));
+        }
+      }
+    },
+
+    // the next 2 reducers have repeated code, to be refactored...
+
+    decreaseProductFromLocal: (state, action) => {
+      const index = state.data.findIndex((item) => item.product._id === action.payload);
+      state.data[index].cartQuantity -= 1;
+      localStorage.setItem('cart', JSON.stringify(state.data));
+    },
+
+    delProductFromLocal: (state, action) => {
+      const index = state.data.findIndex((item) => item.product._id === action.payload);
+      state.data.splice(index, 1);
+      localStorage.setItem('cart', JSON.stringify(state.data));
     },
 
     setIsLoading: (state, action) => {
@@ -22,46 +56,27 @@ const cartSlice = createSlice({
 
 export default cartSlice.reducer;
 
-export const { setCart, setIsLoading } = cartSlice.actions;
+export const { setCart, decreaseProductFromLocal, delProductFromLocal, setIsLoading } = cartSlice.actions;
 
-export const fetchCart = () => async (dispatch) => {
-  dispatch(setIsLoading(true));
-  try {
-    const { data, status } = await axiosConfig('/cart');
-    const { products } = data;
+export const fetchCart = (isLogin) => async (dispatch) => {
+  if (isLogin) {
+    dispatch(setIsLoading(true));
+    try {
+      const { data, status } = await axiosConfig('/cart');
 
-    if (status) {
-      dispatch(setCart(products));
-      dispatch(setIsLoading(false));
+      if (status === 200 && data) {
+        const { products } = data;
+        dispatch(setCart(products));
+        dispatch(setIsLoading(false));
+      }
+    } catch (err) {
+      console.error(err.message);
     }
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-
-export const addProduct = (id) => async (dispatch) => {
-  try {
-    const { data, status } = await axiosConfig.put(`/cart/${id}`);
-    const { products } = data;
-
-    if (status) {
-      dispatch(setCart(products));
+  } else {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    if (cart) {
+      dispatch(setCart(cart));
     }
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-
-export const decreaseProduct = (id) => async (dispatch) => {
-  try {
-    const { data, status } = await axiosConfig.delete(`/cart/product/${id}`);
-    const { products } = data;
-
-    if (status) {
-      dispatch(setCart(products));
-    }
-  } catch (err) {
-    console.error(err.message);
   }
 };
 
@@ -69,7 +84,7 @@ export const deleteCart = () => async (dispatch) => {
   try {
     const { status } = await axiosConfig.delete('/cart');
 
-    if (status) {
+    if (status === 200) {
       dispatch(setCart([]));
     }
   } catch (err) {
@@ -77,19 +92,67 @@ export const deleteCart = () => async (dispatch) => {
   }
 };
 
-export const deleteProduct = (id) => async (dispatch) => {
-  try {
-    const { data, status } = await axiosConfig.delete(`/cart/${id}`);
-    const { products } = data;
+export const addProduct = (itemNo, productId, isLogin) => async (dispatch) => {
+  if (isLogin) {
+    try {
+      const { data, status } = await axiosConfig.put(`/cart/${productId}`);
 
-    if (status) {
-      const { _id } = products[0].product;
-
-      if (products.length === 1 && _id === id) {
-        dispatch(deleteCart());
-      } else dispatch(setCart(products));
+      if (status === 200) {
+        const { products } = data;
+        dispatch(setCart(products));
+      }
+    } catch (err) {
+      console.error(err.message);
     }
-  } catch (err) {
-    console.error(err.message);
+  } else {
+    const data = await getProduct(itemNo);
+    dispatch(setCart(data));
+  }
+};
+
+export const decreaseQuantity = (id, isLogin) => async (dispatch) => {
+  if (isLogin) {
+    try {
+      const { data, status } = await axiosConfig.delete(`/cart/product/${id}`);
+      const { products } = data;
+
+      if (status === 200) {
+        dispatch(setCart(products));
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  } else {
+    dispatch(decreaseProductFromLocal(id));
+  }
+};
+
+export const deleteProduct = (id, isLogin) => async (dispatch) => {
+  if (isLogin) {
+    try {
+      const { data, status } = await axiosConfig.delete(`/cart/${id}`);
+      const { products } = data;
+
+      if (status === 200) {
+        const { _id } = products[0].product;
+
+        if (products.length === 1 && _id === id) {
+          dispatch(deleteCart());
+        } else dispatch(setCart(products));
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  } else {
+    dispatch(delProductFromLocal(id));
+  }
+};
+
+export const migrateCart = () => async (dispatch) => {
+  const cart = JSON.parse(localStorage.getItem('cart'));
+  if (cart) {
+    const { products } = await updateCart(cart);
+    dispatch(setCart(products));
+    localStorage.removeItem('cart');
   }
 };
