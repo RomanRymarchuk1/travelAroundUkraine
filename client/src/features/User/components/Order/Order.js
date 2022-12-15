1ecafeacd2daefc95101483f2d6caa7240c7c2b7
Роман Рымарчук <roman.rymarchuk01@gmail.com>
2022-12-15 17:16:38 +0200

    add render of products

--- client/src/features/User/components/Order/Order.js
+++ client/src/features/User/components/Order/Order.js
@@ -2,32 +2,34 @@ import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { Typography, Accordion, AccordionDetails, AccordionSummary, Box } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Product from '../Product/Product';
 
-// TODO: add tourList render
+const Order = ({ totalSum, orderNo, date, products }) => (
+  <Accordion>
+    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+      <Typography>Order №{orderNo}</Typography>
+    </AccordionSummary>
+    <AccordionDetails>
+      <Box sx={{ marginBottom: 4 }}>
+        {products.map(({ product: { name, currentPrice, itemNo, imageUrls }, cartQuantity }) => (
+          <Product
+            name={name}
+            currentPrice={currentPrice}
+            itemNo={itemNo}
+            image={imageUrls[0]}
+            cartQuantity={cartQuantity}
+            key={itemNo}
+          />
+        ))}
+      </Box>
 
-const Order = ({ totalSum, orderNo, date, products }) => {
-  console.log(products);
-
-  return (
-    <Accordion>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-        <Typography>Order №{orderNo}</Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box>
-          {/* {products.map(({ product: { name, currentPrice, itemNo }, cartQuantity }) => (
-
-        ))} */}
-
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Typography>Date: {date}</Typography>
-            <Typography>Total price: {totalSum}€</Typography>
-          </Box>
-        </Box>
-      </AccordionDetails>
-    </Accordion>
-  );
-};
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography>Date: {date}</Typography>
+        <Typography>Total price: {totalSum}€</Typography>
+      </Box>
+    </AccordionDetails>
+  </Accordion>
+);
 
 Order.propTypes = {
   orderNo: PropTypes.string.isRequired,
