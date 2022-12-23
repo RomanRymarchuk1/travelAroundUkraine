8dbd1ce34bb30d76abad8b3f2ec3c7f9d47d018a
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-12-23 23:52:16 +0200

    refactor: user page - user data and orders components saved into constant - error components saved into constant

--- client/src/pages/UserPage/UserPage.js
+++ client/src/pages/UserPage/UserPage.js
@@ -37,48 +37,53 @@ const UserPage = () => {
     if (!userData) dispatch(fetchUserInfo());
   }, []);
 
+  // components saved into constants
+  const userDataAndOrders = (
+    <>
+      <Typography sx={{ my: '15px', p: 0, textAlign: 'center' }} variant="h2">
+        {userData.firstName} {userData.lastName}
+      </Typography>
+      <TabContext value={value}>
+        <TabList
+          textColor="secondary"
+          indicatorColor="secondary"
+          onChange={handleChange}
+          aria-label="User tabs"
+          centered
+        >
+          <Tab label="User Info" value="1" />
+          <Tab label="Orders" value="2" />
+        </TabList>
+
+        <TabPanel value="1">
+          <UserList userData={userData} />
+        </TabPanel>
+
+        <TabPanel value="2">
+          <UserOrders />
+        </TabPanel>
+      </TabContext>
+      <ButtonContainer />
+    </>
+  );
+
+  const errorContainer = (
+    <Container sx={{ textAlign: 'center' }}>
+      <Typography variant="h2">Error code: {error.status}</Typography>
+      <Typography sx={{ color: 'red', py: 3 }} variant="h3">
+        {error.statusText}
+      </Typography>
+      <Typography>Please move to LogIn page</Typography>
+    </Container>
+  );
+
   return (
     <Container component={Paper} sx={containerSX}>
       {isLoading && <CircularProgress sx={preloaderSX} />}
-      {userData && (
-        <>
-          <Typography sx={{ my: '15px', p: 0, textAlign: 'center' }} variant="h2">
-            {userData.firstName} {userData.lastName}
-          </Typography>
 
-          <TabContext value={value}>
-            <TabList
-              textColor="secondary"
-              indicatorColor="secondary"
-              onChange={handleChange}
-              aria-label="User tabs"
-              centered
-            >
-              <Tab label="User Info" value="1" />
-              <Tab label="Orders" value="2" />
-            </TabList>
+      {userData && userDataAndOrders}
 
-            <TabPanel value="1">
-              <UserList userData={userData} />
-            </TabPanel>
-
-            <TabPanel value="2">
-              <UserOrders />
-            </TabPanel>
-          </TabContext>
-
-          <ButtonContainer />
-        </>
-      )}
-      {error && (
-        <Container sx={{ textAlign: 'center' }}>
-          <Typography variant="h2">Error code: {error.status}</Typography>
-          <Typography sx={{ color: 'red', py: 3 }} variant="h3">
-            {error.statusText}
-          </Typography>
-          <Typography>Please move to LogIn page</Typography>
-        </Container>
-      )}
+      {error && errorContainer}
     </Container>
   );
 };
