f973c5203cb4ff7283f22ff258fa56f3fb107995
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-12-23 22:16:18 +0200

    update: form validation rules - first name min, max and allowed characters rules - last name min, max and allowed characters rules - password min, max and allowed characters rules - phone number only ukranian numbers allowed starting with +380 - login min, max and allowed characters rules - 3 regExp created for the validation

--- client/src/features/signup/data/validationSchema.js
+++ client/src/features/signup/data/validationSchema.js
@@ -1,21 +1,46 @@
 import * as yup from 'yup';
 import formModel from './formModel';
 
-const { firstName, lastName, login, email, password, retypePassword } = formModel;
+const { firstName, lastName, login, email, password, retypePassword, telephone } = formModel;
+
+const nameRegExp = /^[a-zA-Z]+$/;
+const phoneRegExp = /^\+380\d{3}\d{2}\d{2}\d{2}$/;
+const loginAndPassRegExp = /^[a-zA-Z0-9]+$/;
 
 export default [
   yup.object().shape({
-    [firstName.name]: yup.string().required(`${firstName.requiredErrorMsg}`),
-    [lastName.name]: yup.string().required(`${lastName.requiredErrorMsg}`),
+    [firstName.name]: yup
+      .string()
+      .min(2, 'First name must be a minimum of 2 characters')
+      .max(25, 'First name must be a maximum of 25 characters')
+      .matches(nameRegExp, 'Allowed letters are only a-z, A-Z')
+      .required(`${firstName.requiredErrorMsg}`),
+
+    [lastName.name]: yup
+      .string()
+      .min(2, 'Last name must be a minimum of 2 characters')
+      .max(25, 'Last name must be a maximum of 25 characters')
+      .matches(nameRegExp, 'Allowed letters are only a-z, A-Z')
+      .required(`${lastName.requiredErrorMsg}`),
+
+    [telephone.name]: yup.string().matches(phoneRegExp, 'Phone number invalid, please enter a Ukranian phone number'),
   }),
 
   yup.object().shape({
     [email.name]: yup.string().email(`${email.invalidErrorMsg}`).required(`${email.requiredErrorMsg}`),
-    [login.name]: yup.string().required(`${login.requiredErrorMsg}`),
+
+    [login.name]: yup
+      .string()
+      .min(3, 'Login must be a minimum of 3 characters')
+      .max(10, 'Login must be a maximum of 10 characters')
+      .matches(loginAndPassRegExp, 'Allowed characters are only a-z, A-Z, 0-9')
+      .required(`${login.requiredErrorMsg}`),
+
     [password.name]: yup
       .string()
-      .min(3, 'Password must be minimum of 3 symbols')
-      .max(12, 'Password must be maximum of 12 symbols')
+      .min(7, 'Password must be a of minimum of 7 characters')
+      .max(30, 'Password must be maximum of 30 characters')
+      .matches(loginAndPassRegExp, 'Allowed characters are only a-z, A-Z, 0-9')
       .required(`${password.requiredErrorMsg}`),
 
     [retypePassword.name]: yup
