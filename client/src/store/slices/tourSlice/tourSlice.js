a981e1f27c907067e80ee6a12978f3561adb8237
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2022-12-25 00:14:59 +0200

    update: tour slice re-written - thunk function re-written with createAsyncThunk toolkit function

--- client/src/store/slices/tourSlice/tourSlice.js
+++ client/src/store/slices/tourSlice/tourSlice.js
@@ -1,28 +1,50 @@
-/* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-unused-vars */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import getProduct from '../../../api/getProduct';
+
+// export const { setTour } = tourSlice.actions;
+
+// export const fetchTour = (itemNo) => async (dispatch) => {
+//   const data = await getProduct(itemNo);
+
+//   dispatch(setTour(data));
+// };
 
 const initialState = {
   data: {},
+  isLoading: false,
+  error: null,
 };
+
+export const fetchTour = createAsyncThunk('order/fetchTour', async (itemNo, { rejectWithValue }) => {
+  try {
+    const data = await getProduct(itemNo);
+    return data;
+  } catch (err) {
+    return rejectWithValue('Something wrong happened, please reload the page or try again later.');
+  }
+});
 
 const tourSlice = createSlice({
   name: 'tour',
   initialState,
 
-  reducers: {
-    setTour: (state, action) => {
+  extraReducers: (builder) => {
+    builder.addCase(fetchTour.pending, (state) => {
+      state.error = null;
+      state.isLoading = true;
+    });
+
+    builder.addCase(fetchTour.fulfilled, (state, action) => {
       state.data = action.payload;
-    },
+      state.isLoading = false;
+    });
+
+    builder.addCase(fetchTour.rejected, (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    });
   },
 });
-
-export const { setTour } = tourSlice.actions;
-
-export const fetchTour = (itemNo) => async (dispatch) => {
-  const data = await getProduct(itemNo);
-
-  dispatch(setTour(data));
-};
 
 export default tourSlice.reducer;
