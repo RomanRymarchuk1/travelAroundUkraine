d6a974bfda22a8b291cda6e89fd56338115154aa
Oleksandr Altynnikov <siolent.ou@gmail.com>
2023-01-13 11:26:48 +0200

    Co-authored-by: HIMDanny <HIMDanny@users.noreply.github.com>

--- client/src/store/slices/catalogueSlice/catalogueSlice.js
+++ client/src/store/slices/catalogueSlice/catalogueSlice.js
@@ -1,7 +1,34 @@
 /* eslint-disable no-return-assign */
 /* eslint-disable no-param-reassign */
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axiosConfig from '../../../axiosConfig';
+
+export const fetchCatalogue = createAsyncThunk('products/fetchCatalogue', async () => {});
+
+export const fetchCatalogueProducts = createAsyncThunk(
+  'products/fetchCatalogueProducts',
+  async (page, { rejectWithValue }) => {
+    try {
+      const { data, status } = await axiosConfig.get(`products/filter?perPage=5&startPage=${page}`);
+      if (status >= 200 && status <= 300) {
+        return data;
+      }
+    } catch (err) {
+      return rejectWithValue(err);
+    }
+  }
+);
+
+export const fetchPopularProducts = createAsyncThunk('products/isPopular', async (_, {rejectWithValue}) => {
+  try {
+    const { status, data } = await axiosConfig('/products/filter?isPopular=true');
+    if (status >= 200 && status <= 300) {
+      return data.products;
+    }
+  } catch (err) {
+    return rejectWithValue(err);
+  }
+});
 
 const catalogueSlice = createSlice({
   name: 'products',
@@ -9,44 +36,41 @@ const catalogueSlice = createSlice({
     products: [],
     popular: [],
     isLoading: false,
+    error: null,
+    totalPages: null,
+    currentPage: 1,
   },
   reducers: {
-    setProducts: (state, action) => {
-      state.products = action.payload;
-    },
-
     setPopular: (state, action) => {
       state.popular = action.payload;
     },
-
-    setIsLoading: (state, action) => {
-      state.isLoading = action.payload;
+    setCurrentPage: (state, action) => {
+      state.currentPage = Number(action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCatalogueProducts.pending, (state) => {
+      state.isLoading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchCatalogueProducts.fulfilled, (state, action) => {
+      const { products, productsQuantity } = action.payload;
+      state.isLoading = false;
+      state.products = products;
+      state.totalPages = productsQuantity;
+    });
+    builder.addCase(fetchCatalogueProducts.rejected, (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    });
+    builder.addCase(fetchPopularProducts.fulfilled, (state, action) => {
+      state.popular = action.payload;
+    });
+    builder.addCase(fetchPopularProducts.rejected, (state, action) => {
+      state.error = action.payload;
+    });
   },
 });
 
+export const { setCurrentPage } = catalogueSlice.actions;
 export default catalogueSlice.reducer;
-
-export const { setProducts, setPopular, setIsLoading } = catalogueSlice.actions;
-
-export const getProducts = () => async (dispatch) => {
-  try {
-    const { data, status } = await axios('/products');
-
-    if (status) {
-      dispatch(setProducts(data));
-      dispatch(setPopular(data));
-    }
-  } catch (err) {
-    console.error(err.message);
-  }
-};
-
-export const getPopularProducts = () => async (dispatch) => {
-  const { data, status } = await axios('/products/filter?isPopular=true');
-
-  if (status) {
-    const { products } = data;
-    dispatch(setPopular(products));
-  }
-};
