f9ad07b707e4b496b37142653ede037384190854
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-01-25 21:12:16 +0200

    update HeaderFilterItems component updated - items passed dynamically after receiving search results - styles slightly altered for better design and UI - component refactored and map results saved in constants - UI adjusted to display different results depending on state.

--- client/src/layout/header/components/HeaderFilterItems/HeaderFilterItems.js
+++ client/src/layout/header/components/HeaderFilterItems/HeaderFilterItems.js
@@ -1,61 +1,59 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { Box, Paper, MenuList, MenuItem, Typography } from '@mui/material';
-import { styled } from '@mui/system';
+import { Box, Paper, MenuList, MenuItem, Typography, styled } from '@mui/material';
+import { useSelector, shallowEqual } from 'react-redux';
 
-const items = [
-    {
-        image: '../../assets/images/headerFilter/1.jpeg',
-        name: 'karpaty - 1 day + spa',
-        price: ' 4000 uah',
-        amountOfDay: '1 day',
-    },
-    {
-        image: '../../assets/images/headerFilter/2.jpeg',
-        name: 'winter karpaty',
-        price: ' 3500 uah',
-        amountOfDay: '2 day',
-    },
-    {
-        image: '../../assets/images/headerFilter/3.jpeg',
-        name: 'weel in karpaty',
-        price: ' 7000 uah',
-        amountOfDay: '7 day',
-    },
-];
+const ItemBox = styled(Box)({
+  display: 'grid',
+  gridTemplateColumns: 'auto auto',
+  gridTemplateRows: 'repeat(2, 1fr)',
+  gridColumnGap: '15px',
+  gridRowGap: '15px',
+  boxSizing: 'border-box',
+});
 
-const ItemBox = styled((props) => <Box  {...props} />)(() => ({
-    paddingLeft: 0,
-    display: 'grid',
-    gridTemplateColumns: 'auto auto',
-    gridTemplateRows: 'repeat(2, 1fr)',
-    gridColumnGap: ' 10px',
-    gridRowGap: '0px',
-    boxSizing: 'border-box'
-}));
+const HeaderFilterItems = () => {
+  const { data, error } = useSelector((store) => store.searchBar, shallowEqual);
 
-const HeaderFilterItems= () => {
-    return (
-        <Paper sx={{ width: '242px', borderRadius: '5px', position: 'absolute'}}>
-            <Typography sx={{ paddingTop: '14px', paddingLeft: '10px' }}>found: 3</Typography>
-            {items.map(({ image, name, price, amountOfDay }) => (
-                <MenuList key={name}>
-                    <MenuItem sx={{ paddingLeft: 1 }}>
-                        <ItemBox>
-                            <Box sx={{ gridArea: '1 / 1 / 3 / 3' }}>
-                                <img src={image} alt="karpaty" />
-                            </Box>
-                            <Box sx={{ gridArea: '1 / 3 / 2 / 5' }}>{name}</Box>
-                            <Box sx={{ gridArea: '2 / 3 / 3 / 4' }}>{price}</Box>
-                            <Box sx={{ gridArea: '2 / 4 / 3 / 5' }}>{amountOfDay}</Box>
-                        </ItemBox>
-                    </MenuItem>
-                </MenuList>
-            ))}
-            <Typography align='center'>no results.</Typography>
-            <Typography align='center' sx={{ marginBottom: '15px' }}>please, change your request</Typography>
-        </Paper>
-    )
-}
+  const searchBarList = data.map(({ imageUrls, name, currentPrice, season }) => (
+    <MenuItem key={name}>
+      <ItemBox>
+        <Box sx={{ gridArea: '1 / 1 / 3 / 3' }}>
+          <img src={imageUrls[0]} alt={name} width="90px" height="50px" />
+        </Box>
+        <Box sx={{ gridArea: '1 / 3 / 2 / 5' }}>{name}</Box>
+        <Box sx={{ gridArea: '2 / 3 / 3 / 4' }}>{currentPrice}€</Box>
+        <Box sx={{ gridArea: '2 / 4 / 3 / 5' }}>{season}</Box>
+      </ItemBox>
+    </MenuItem>
+  ));
+
+  const searchResults = (
+    <>
+      <Typography align="center" sx={{ paddingTop: '14px' }} variant="h3">
+        Search results: {data.length}
+      </Typography>
+      <MenuList>{searchBarList}</MenuList>
+    </>
+  );
+
+  return (
+    <Paper sx={{ borderRadius: '5px', position: 'absolute', minWidth: '200px', padding: '20px' }}>
+      {data.length ? searchResults : null}
+
+      {!data.length && !error && (
+        <Typography variant="h3" align="center" gutterBottom={false}>
+          No results found
+        </Typography>
+      )}
+
+      {error && (
+        <Typography align="center" gutterBottom={false} sx={{ color: 'error.main' }}>
+          {error}
+        </Typography>
+      )}
+    </Paper>
+  );
+};
 
 export default HeaderFilterItems;
