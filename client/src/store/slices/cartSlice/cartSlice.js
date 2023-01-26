654c4cd9e3e52d34656c63df53614349433536f7
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-01-26 21:59:22 +0200

    feature: cart page snackBar added - snackBar implemented in cart page - snackbar state changed in all cart slice functions

--- client/src/store/slices/cartSlice/cartSlice.js
+++ client/src/store/slices/cartSlice/cartSlice.js
@@ -8,6 +8,11 @@ const initialState = {
   data: [],
   isLoading: false,
   error: null,
+  snackBar: {
+    isSnackBarOpen: false,
+    severity: 'success',
+    text: '',
+  },
 };
 
 /**
@@ -112,6 +117,10 @@ const cartSlice = createSlice({
         state.data[index].cartQuantity += 1;
         writeToLocalStorage('cart', state.data);
       }
+
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product added successfully';
+      state.snackBar.severity = 'success';
     },
 
     deleteCart: (state) => {
@@ -122,12 +131,24 @@ const cartSlice = createSlice({
       const index = state.data.findIndex((item) => item.product._id === action.payload);
       state.data[index].cartQuantity -= 1;
       writeToLocalStorage('cart', state.data);
+
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product removed successfully';
+      state.snackBar.severity = 'success';
     },
 
     delProductFromLocal: (state, action) => {
       const index = state.data.findIndex((item) => item.product._id === action.payload);
       state.data.splice(index, 1);
       writeToLocalStorage('cart', state.data);
+
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product deleted successfully';
+      state.snackBar.severity = 'success';
+    },
+
+    closeSnackBar: (state) => {
+      state.snackBar.isSnackBarOpen = false;
     },
   },
 
@@ -176,11 +197,17 @@ const cartSlice = createSlice({
         state.data = products;
       }
       state.isLoading = false;
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product added successfully';
+      state.snackBar.severity = 'success';
     });
 
     builder.addCase(addProduct.rejected, (state, action) => {
       state.error = action.payload;
       state.isLoading = false;
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Something went wrong';
+      state.snackBar.severity = 'error';
     });
 
     builder.addCase(decreaseQuantity.pending, (state) => {
@@ -192,11 +219,18 @@ const cartSlice = createSlice({
       const { products } = action.payload;
       state.data = products;
       state.isLoading = false;
+
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product removed successfully';
+      state.snackBar.severity = 'success';
     });
 
     builder.addCase(decreaseQuantity.rejected, (state, action) => {
       state.error = action.payload;
       state.isLoading = false;
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Something went wrong';
+      state.snackBar.severity = 'error';
     });
 
     builder.addCase(deleteProduct.pending, (state) => {
@@ -214,11 +248,19 @@ const cartSlice = createSlice({
       if (products.length === 1 && _id === arg) {
         state.data = [];
       } else state.data = products;
+
+      state.isLoading = false;
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Product deleted successfully';
+      state.snackBar.severity = 'success';
     });
 
     builder.addCase(deleteProduct.rejected, (state, action) => {
       state.error = action.payload;
       state.isLoading = false;
+      state.snackBar.isSnackBarOpen = true;
+      state.snackBar.text = 'Something went wrong';
+      state.snackBar.severity = 'error';
     });
   },
 });
@@ -231,6 +273,7 @@ export const {
   deleteCart,
   decreaseProductFromLocal,
   delProductFromLocal,
+  closeSnackBar,
 } = cartSlice.actions;
 
 // export const migrateCart = () => async (dispatch) => {
