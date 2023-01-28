ad9890733fa3e39349511c834781c29a3acefb8f
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-01-29 00:53:21 +0200

    update: ContactsForm component - unneeded form inputs removed - unneeded styled components removed - states created for AlertModal comopnent and passed as props - handleSubmit function calls subscribeUser api function - handleSubmit opens success or error AlertModal after checking reply

--- client/src/features/contactUs/components/ContactsForm/ContactsForm.js
+++ client/src/features/contactUs/components/ContactsForm/ContactsForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Button, styled } from '@mui/material';
+import { Button, styled, Typography } from '@mui/material';
 import { TextField } from 'formik-mui';
 import { Formik, Form, Field } from 'formik';
 import * as yup from 'yup';
 import { AlertModal } from '../../../../components';
+import subscribeUser from '../../../../api/subscribeUser';
 
 const TextFieldContactsPersonData = styled(Field)(({ theme }) => ({
-  marginRight: '10px',
   width: '270px',
 
   [theme.breakpoints.up('tablet')]: {
@@ -17,73 +17,57 @@ const TextFieldContactsPersonData = styled(Field)(({ theme }) => ({
   },
 }));
 
-const TextFieldContacts = styled(Field)(({ theme }) => ({
-  width: '350px',
-  display: 'block',
-
-  [theme.breakpoints.up('tablet')]: {
-    width: '570px',
-  },
-  [theme.breakpoints.up('laptop')]: {
-    width: '610px',
-  },
-}));
-
 const ContactsForm = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalText, setModalText] = useState('');
+  const [modalSuccess, setModalSuccess] = useState('true');
+
   const closeModal = () => setIsModalOpen(false);
 
-  const handleSubmit = (values, { resetForm }) => {
-    console.log(values);
-    setIsModalOpen(true);
-    resetForm();
-  };
-
   const initialValues = {
-    name: '',
     email: '',
-    message: '',
   };
 
   const schema = yup.object().shape({
-    name: yup.string('Enter your Name').required('Name is required'),
     email: yup.string('Enter your email').email('Enter a valid email').required('Email is required'),
-    message: yup.string('Enter your Message').required('Message is required'),
   });
 
-  const modalSubmit = () => {
-    closeModal();
+  const handleSubmit = async (values) => {
+    const { isSuccess, error } = await subscribeUser(values);
+    if (isSuccess) {
+      setIsModalOpen(true);
+      setModalTitle('Subscription Successful');
+      setModalText('You can enjoy our Newsletter service now on your mail');
+      setModalSuccess('true');
+    } else {
+      setIsModalOpen(true);
+      setModalTitle('Subscription failed');
+      setModalText(error.message);
+      setModalSuccess('false');
+    }
   };
 
   return (
     <Formik initialValues={initialValues} validationSchema={schema} onSubmit={handleSubmit}>
       {() => (
         <Form>
-          <TextFieldContactsPersonData component={TextField} name="name" label="Name" margin="normal" />
           <TextFieldContactsPersonData component={TextField} name="email" label="Email" margin="normal" />
-          <TextFieldContacts
-            component={TextField}
-            name="message"
-            label="Message"
-            multiline
-            fullWidth
-            margin="normal"
-            rows={8}
-          />
 
-          <Button margin="normal" sx={{ display: 'block', marginTop: '15px' }} color="primary" type="submit">
-            Send
+          <Button margin="normal" sx={{ display: 'block', marginTop: '15px' }} type="submit">
+            Subscribe
           </Button>
+
           <AlertModal
             open={isModalOpen}
-            title="Success"
-            success
-            submitButtonText="OK"
-            disableCancelButton
             onClose={closeModal}
-            onSubmit={modalSubmit}
+            onSubmit={closeModal}
+            title={modalTitle}
+            disableCancelButton
+            submitButtonText="Close"
+            success={modalSuccess}
           >
-             Thank you for your message. we will write you as soon as possible!
+            <Typography>{modalText}</Typography>
           </AlertModal>
         </Form>
       )}
