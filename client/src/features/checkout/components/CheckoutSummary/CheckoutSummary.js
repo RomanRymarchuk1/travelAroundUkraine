5e9244a0c37f4e4974fbe6f093991caf76f98456
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-02-01 14:52:55 +0200

    feature add route: - add route to catalogue page on continue shopping btn after checkout

--- client/src/features/checkout/components/CheckoutSummary/CheckoutSummary.js
+++ client/src/features/checkout/components/CheckoutSummary/CheckoutSummary.js
@@ -1,11 +1,16 @@
-import React,{memo} from 'react';
+import React, { memo } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Typography, Button } from '@mui/material';
 
-const CheckoutSummary = () => (
-  <>
-    <Typography variant="h2">Order Summary</Typography>
-    <Button>Continue Shopping</Button>
-  </>
-);
+const CheckoutSummary = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Typography variant="h2">Order Summary</Typography>
+      <Button onClick={() => navigate('/catalogue')}>Continue Shopping</Button>
+    </>
+  );
+};
 
 export default memo(CheckoutSummary);
